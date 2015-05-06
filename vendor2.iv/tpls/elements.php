cfecc9e9b21c8846e2cee6368e0da4e73a6fe90d
Ivan <auzp@ukr.net>
2015-05-06 21:10:45 +0300

    icons

--- vendor2.iv/tpls/elements.php
+++ vendor2.iv/tpls/elements.php
@@ -11,7 +11,115 @@
         <link href="../css/styles.css" rel="stylesheet" type="text/css"/>    
     </head>
     <body>
-        <h1>Elements</h1>
+        <div class="elements">
+            <h1>ICONS</h1>
+            <hr>
+            <i class="icon-page-arrow"></i>
+            <i class="icon-slidearrow-left-large"></i>
+            <i class="icon-slidearrow-right-large"></i>
+            
+            <i class="icon-accarrow-down"></i>
+            <i class="icon-accarrow-up"></i>
+            <i class="icon-checkbox"></i>
+            <i class="icon-selectarrow-small"></i>
+            
+            <i class="icon-close"></i>
+            
+            <i class="icon-likefinger"></i>
+            <i class="icon-selectarrow-middle"></i>
+            
+            <i class="icon-topbar-compose"></i>
+            <i class="icon-topbar-user"></i>
+            <i class="icon-topbar-cart"></i>
+            <i class="icon-topbar-checkout"></i>
+            <i class="icon-flag-uk"></i>
+            <i class="icon-flag-it"></i>
+            <i class="icon-flag-ge"></i>
+            <i class="icon-flag-fr"></i>
+            <i class="icon-flag-sp"></i>
+            <i class="icon-menu-minus"></i>
+            <i class="icon-menu-plus"></i>
+            <i class="icon-grid"></i>
+            <i class="icon-list"></i>
+                        
+            <i class="icon-selectarrow-large"></i>
+            
+            <i class="icon-product-check"></i>
+            <i class="icon-product-like"></i>
+            <i class="icon-support-mail"></i>
+            <i class="icon-support-skype"></i>
+            
+            <i class="icon-blog-cloud"></i>
+            <i class="icon-blog-eye"></i>
+            <i class="icon-blog-folder"></i>
+            <i class="icon-blog-like"></i>
+            <i class="icon-blog-user"></i>
+            <i class="icon-contact-mail"></i>
+            <i class="icon-contact-mobile"></i>
+            <i class="icon-contact-phone"></i>
+            <i class="icon-contact-skype"></i>
+            <i class="icon-search"></i>
+            <i class="icon-social-delicious"></i>
+            <i class="icon-social-facebook"></i>
+            <i class="icon-social-flickr"></i>
+            <i class="icon-social-linkedin"></i>
+            <i class="icon-social-mail"></i>
+            <i class="icon-social-rss"></i>
+            <i class="icon-social-skype"></i>
+            <i class="icon-social-twitter"></i>
+            <i class="icon-world"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            <i class="icon-"></i>
+            
+            <hr>
+            <h1>FORM ELEMENTS</h1>
+            
+            
+            
+            
+            
+            
+            
+            
+            <hr>
+        </div>        
     </body>
 </html>
 
