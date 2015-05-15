01eeb0c1966f7e1c42e9cc8a394ecaf058cdca6c
Ivan <auzp@ukr.net>
2015-05-15 11:59:27 +0300

    label

--- vendor2.iv/tpls/_work/elements/tabs/labels.php
+++ vendor2.iv/tpls/_work/elements/tabs/labels.php
@@ -1,25 +1,16 @@
 <span class="lbl lbl-circle">
     <span>$1450</span>
+</span>                       
+<span class="lbl lbl-circle lbl-product">
+    <span>$378</span>
 </span>
-
-<div class="elastislide">                       
-    <span class="lbl lbl-circle">
-        <span>$378</span>
-    </span>
-</div>
-<div class="thumbnail">
-    <span class="lbl lbl-circle">
-        <span>
-            <dl>
-                <dt>$870</dt>
-                <dd>$710</dd>
-            </dl>
-        </span>
-    </span>
-    <span class="lbl lbl-circle lbl-disable">
-        <span>$378</span>
-    </span>
-</div>
+<span class="lbl lbl-circle lbl-thumb">
+    $710<br>
+    <strong>$870</strong>   
+</span>
+<span class="lbl lbl-circle lbl-thumb lbl-disable">
+    <span>$378</span>
+</span>
 <span class="lbl lbl-sm lbl-new">NEW</span>
 <span class="lbl lbl-sm lbl-discount">-30%</span>
 <div class="lbl lbl-contact"><i class="icon-contact-phone"></i></div>
