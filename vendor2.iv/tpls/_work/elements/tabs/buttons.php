d36cf3cfce342067abec0db0d9559081f9c2e0c3
Ivan <auzp@ukr.net>
2015-05-15 11:13:55 +0300

    buttons

--- vendor2.iv/tpls/_work/elements/tabs/buttons.php
+++ vendor2.iv/tpls/_work/elements/tabs/buttons.php
@@ -1,21 +1,20 @@
+<h2 class="_h">Butttons</h2>
 <button><strong class="btn btn-add-lg"><span>ADD TO CART</span></strong></button>
-<a href="#" class="btn btn-account"><span>CREATE MY ACCOUNT</span></a>
-<a href="#" class="btn btn-more"><span>LEARN MORE</span></a>
 <button><strong class="btn btn-submit"><span>SUBMIT</span></strong></button>
-<button><strong class="btn btn-add"><span>ADD TO CART</span></strong></button>
-<button><strong class="btn btn-disabled"><span>ADD TO CART</span></strong></button>
 <button><strong class="btn btn-compare"><span>COMPARE</span></strong></button>
 <button><strong class="btn btn-range"><span>OK</span></strong></button>                        
-<hr>
-<button><strong class="btn btn-check"></strong></button>
-<button><strong class="btn btn-like"></strong></button>
-<button><strong class="btn btn-hidden-add"></strong></button>
-<button><strong class="btn btn-hidden-menu"></strong></button>
 <button><strong class="btn btn-search"></strong></button>
-<button><strong class="btn btn-cart"><span>0 item(s) - $0.00</span></strong></button>
-<button><strong class="btn btn-close"></strong></button>  
-<button><strong class="btn btn-grid"></strong></button>
-<button><strong class="btn btn-list"></strong></button>
+<h2 class="_h">Links</h2>
+<a href="#" class="btn btn-account"><span>CREATE MY ACCOUNT</span></a>
+<a href="#" class="btn btn-more"><span>LEARN MORE</span></a>
+<a href="#" class="btn btn-add"><span>ADD TO CART</span></a>
+<a href="#" class="btn btn-disabled"><span>ADD TO CART</span></a>
+<a href="#" class="btn btn-check"></a>
+<a href="#" class="btn btn-like"></a>
+<a href="#" class="btn btn-hidden-add"></a>
+<a href="#" class="btn btn-hidden-menu"></a>
+<a href="#" class="btn btn-cart"><span>0 item(s) - $0.00</span></a>
+<a href="#" class="btn btn-close"></a> 
 <hr>
 <a href="#" class="btn btn-social btn-facebook"></a>
 <a href="#" class="btn btn-social btn-twitter"></a>
@@ -30,3 +29,6 @@
 <a href="#" class="btn btn-post btn-twitter"></a>
 <a href="#" class="btn btn-post btn-rss"></a>
 <a href="#" class="btn btn-post btn-mail"></a>
+<h2 class="_h">Divs</h2>
+<div><strong class="btn btn-grid"></strong></div>
+<div><strong class="btn btn-list"></strong></div>
