c8b91c6b83611eb3e688dc66f583d1c079ac9895
Ivan <auzp@ukr.net>
2015-05-15 12:16:18 +0300

    label

--- vendor2.iv/tpls/_work/elements/tabs/labels.php
+++ vendor2.iv/tpls/_work/elements/tabs/labels.php
@@ -1,17 +1,24 @@
-<span class="lbl lbl-circle">
+<div class="lbl lbl-circle">
     <span>$1450</span>
-</span>                       
-<span class="lbl lbl-circle lbl-product">
+</div>                       
+<div class="lbl lbl-circle lbl-product">
     <span>$378</span>
-</span>
-<span class="lbl lbl-circle lbl-thumb">
-    $710<br>
-    <strong>$870</strong>   
-</span>
-<span class="lbl lbl-circle lbl-thumb lbl-disable">
+</div>
+<div class="lbl lbl-circle lbl-thumb">
+    <span>
+        $710<br>
+        <strong>$870</strong>
+    </span>  
+</div>
+<div class="lbl lbl-circle lbl-thumb lbl-disable">
     <span>$378</span>
-</span>
-<span class="lbl lbl-sm lbl-new">NEW</span>
-<span class="lbl lbl-sm lbl-discount">-30%</span>
+</div>
+<div class="lbl lbl-sm lbl-new">NEW</div>
+<div class="lbl lbl-sm lbl-discount">-30%</div>
 <div class="lbl lbl-contact"><i class="icon-contact-phone"></i></div>
-<div class="lbl lbl-date">15 <strong>AUG</strong></div>
+<div class="lbl lbl-date">
+    <span>
+        15<br>
+        <strong>AUG</strong>
+    </span>
+</div>
