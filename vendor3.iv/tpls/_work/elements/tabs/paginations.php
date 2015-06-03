671ae79812103c0947a13ded5ecbab1e793c3dbb
Ivan <auzp@ukr.net>
2015-06-03 17:50:19 +0300

    commit bottom

--- vendor3.iv/tpls/_work/elements/tabs/paginations.php
+++
@@ -1,8 +0,0 @@
-<ul class="pagination clearfix">
-    <li class="active"><a href="#">1</a></li>
-    <li><a href="#">2</a></li>
-    <li><a href="#">3</a></li>
-    <li><a href="#">4</a></li>
-    <li><a href="#">5</a></li>
-    <li class="next"><a href="#"><i class="icon-page-arrow"></i></a></li>
-</ul>
--- vendor3.iv/tpls/home.php
+++ vendor3.iv/tpls/home.php
@@ -13,44 +13,72 @@
         <link type="text/css" rel="stylesheet" href="/css/styles.css">    
     </head>
     <body>
-        <div class="top-bar-wrap">
-            <div class="top-bar">
-                <div class="top-bar-links">
-                    <a href="#" data-tooltip="MyWishlist(0)"><i class="icon-topbar-compose"></i>My Wishlist (0)</a>
-                    <a href="#" data-tooltip="My&nbsp;Account"><i class="icon-topbar-user"></i>My Account</a>
-                    <a href="#" data-tooltip="Shopping&nbsp;Cart"><i class="icon-topbar-cart"></i>Shopping Cart</a>
-                    <a href="#" data-tooltip="Checkout"><i class="icon-topbar-checkout"></i>Checkout</a>
-                </div>
-                <div class="top-bar-drops">
-                    <p><span class="hidden-desktop">Welcome visitor you can </span><a href="#" class="login">login</a> or <a href="#">create an account</a>.</p>                        
-                    <div class="top-item currency">
-                        <span class="hidden-phone">US Dollar</span><span class="visible-phone">USD</span>
-                    </div> 
-                    <div class="top-item language">
-                        <i class="icon-flag-uk"></i><span class="hidden-phone">English</span><span class="visible-phone">En</span>
+        <header>
+            <div class="top-bar-wrap">
+                <div class="top-bar">
+                    <div class="top-bar-links">
+                        <a href="#" data-tooltip="MyWishlist(0)"><i class="icon-topbar-compose"></i>My Wishlist (0)</a>
+                        <a href="#" data-tooltip="My&nbsp;Account"><i class="icon-topbar-user"></i>My Account</a>
+                        <a href="#" data-tooltip="Shopping&nbsp;Cart"><i class="icon-topbar-cart"></i>Shopping Cart</a>
+                        <a href="#" data-tooltip="Checkout"><i class="icon-topbar-checkout"></i>Checkout</a>
                     </div>
-                </div>
-            </div>            
-        </div>
-        <div class="header-top">
-            <a href="#" class="logo"><strong>Venedor</strong><span>.</span></a> 
-            <div class="support">
-                <span class="lbl lbl-support">
-                    <i class="icon-ears"></i>
-                    +(404) 158 14 25 78<br>
-                    +(404) 851 21 48 15
-                </span>
-                <span class="lbl lbl-support alt">
-                    <span>
-                        <i class="icon-support-skype"></i>                                   
-                        <a href="#">venedor_support</a>
-                    </span>
-                    <span>
-                        <i class="icon-support-mail"></i>
-                        <a href="mailto:venedor@gmail.com">venedor@gmail.com</a>
-                    </span>
-                </span> 
+                    <div class="top-bar-drops">
+                        <p><span class="hidden-desktop">Welcome visitor you can </span><a href="#" class="login">login</a> or <a href="#">create an account</a>.</p>                        
+                        <div class="top-item currency">
+                            <span class="hidden-phone">US Dollar</span><span class="visible-phone">USD</span>
+                        </div> 
+                        <div class="top-item language">
+                            <i class="icon-flag-uk"></i><span class="hidden-phone">English</span><span class="visible-phone">En</span>
+                        </div>
+                    </div>
+                </div>            
             </div>
-        </div>                
+            <div class="header-top">
+                <a href="#" class="logo"><strong>Venedor</strong><span>.</span></a> 
+                <div class="support">
+                    <span class="lbl lbl-support">
+                        <i class="icon-ears"></i>
+                        +(404) 158 14 25 78<br>
+                        +(404) 851 21 48 15
+                    </span>
+                    <span class="lbl lbl-support alt">
+                        <span>
+                            <i class="icon-support-skype"></i>                                   
+                            <a href="#">venedor_support</a>
+                        </span>
+                        <span>
+                            <i class="icon-support-mail"></i>
+                            <a href="mailto:venedor@gmail.com">venedor@gmail.com</a>
+                        </span>
+                    </span> 
+                </div>
+            </div>  
+            <div class="header-bottom">
+                <nav>
+                    <div class="nav-full">
+                        <a href="#">fashion</a>
+                        <a href="#">electronics</a>
+                        <a href="#">gifts</a>
+                        <a href="#">books</a>
+                        <a href="#">at home</a>
+                        <a href="#">music</a>
+                    </div>                    
+                    <div class="nav-short">
+                        <input type="checkbox" id="menu" name="menu">
+                        <label for="menu">
+                            <span>menu</span>
+                            <div class="btn btn-hidden-menu"><i class="icon-hidden-menu"></i></div>
+                        </label>
+                    </div>
+                </nav>
+                <div class="quick-access">
+                    <form action="#" method="post" class="search">
+                        <button><span class="btn btn-search"><i class="icon-search"></i></span></button>   
+                        <input type="search" class="input-search" placeholder="Search here">                        
+                    </form>
+                    <a href="#" class="btn btn-cart"><i class="icon-cart"></i> <em>0 item(s) - $0.00</em></a>
+                </div>
+            </div>
+        </header>
     </body>
 </html>
