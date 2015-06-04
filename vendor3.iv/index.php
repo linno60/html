f632d999ab869092f5af52a8da6de0c4981d67f6
Ivan <auzp@ukr.net>
2015-06-04 20:05:11 +0300

    footer commit

--- vendor3.iv/index.php
+++ vendor3.iv/index.php
@@ -5,20 +5,7 @@
 
     switch ($page) 
     {
-        case 'index': $layout = '_work/index'; break; 
-        case 'elements': $layout = '_work/elements/elements'; break; 
-        case 'plugins': $layout = '_work/plugins'; break; 
-        case 'home': $layout = 'home'; break;
-        case 'category-grid-list': $layout = 'category-grid-list'; break;       
-        case 'product': $layout = 'product'; break;
-        case 'shopping-cart': $layout = 'shopping-cart'; break;
-        case 'compare-products': $layout = 'compare-products'; break;
-        case 'contact-us': $layout = 'contact-us'; break;
-        case 'register-account': $layout = 'register-account'; break;
-        case 'checkout': $layout = 'checkout'; break;
-        case 'blog': $layout = 'blog'; break;
-        case 'blog-post': $layout = 'blog-post'; break;
-        default: $layout = '404';
+        case 'index': $layout = 'home'; break; 
     }
 
     require_tpl($layout);
--- vendor3.iv/tpls/home.php
+++ vendor3.iv/tpls/home.php
@@ -80,5 +80,109 @@
                 </div>
             </div>
         </header>
+        <div class="slider">
+            <span class="slider-arrow left"><i class="icon-sliderarrow-left"></i></span>
+            <span class="slider-arrow right"><i class="icon-sliderarrow-right"></i></span>
+        </div>
+        <footer>
+            <div class="footer-wrap">
+            <div class="footer">
+                <div>
+                    <h5>my account</h5>
+                    <ul class="list list-lnk list-footer">
+                        <li><a href="#">My account</a></li>
+                        <li><a href="#">Personal information</a></li>
+                        <li><a href="#">Addresses</a></li>
+                        <li><a href="#">Discount</a></li>
+                        <li><a href="#">Orders history</a></li>
+                        <li><a href="#">Your Vouchers</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5>information</h5>
+                    <ul class="list list-lnk list-footer">
+                        <li><a href="#">New products</a></li>
+                        <li><a href="#">Top sellers</a></li>
+                        <li><a href="#">Specials</a></li>
+                        <li><a href="#">Manufacturers</a></li>
+                        <li><a href="#">Suppliers</a></li>
+                        <li><a href="#">Our stores</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5>contact information</h5>
+                    <h6>venedor ltd</h6>
+                    <address>
+                        <span>United Kingdom</span>
+                        <span>Greater London</span>
+                        <span>London  02587</span>
+                        <span>Oxford Street 48/188</span>
+                        <dl>
+                            <dt>Working days: </dt><dd>Mon. - Sun.</dd>
+                            <dt>Working hours: </dt><dd>9.00AM - 8.00PM</dd>
+                        </dl>
+                    </address>
+                </div>
+                <div>
+                    <h5>facebook</h5>
+                    <div class="like-box-wrap">
+                        <span class="likes">1452 people like <a href="#">PromoKit</a></span>
+                        <button><span class="btn btn-likefinger"><i class="icon-likefinger"></i><em>Like!</em></span></button>
+                        <div class="like-box">
+                            <a href="#">
+                                <img src="/images/shape4.png" width="60" height="60" alt="">
+                                <span>Anna</span>
+                            </a>
+                            <a href="#">
+                                <img src="/images/shape4.png" width="60" height="60" alt="">
+                                <span>Tom</span>
+                            </a>
+                            <a href="#">
+                                <img src="/images/shape4.png" width="60" height="60" alt="">
+                                <span>Antuan</span>
+                            </a>
+                            <a href="#">
+                                <img src="/images/shape4.png" width="60" height="60" alt="">
+                                <span>Milla</span>
+                            </a>
+                            <a href="#">
+                                <img src="/images/shape4.png" width="60" height="60" alt="">
+                                <span>Justine</span>
+                            </a>
+                            <a href="#">
+                                <img src="/images/shape4.png" width="60" height="60" alt="">
+                                <span>Maria</span>
+                            </a>
+                            <a href="#">
+                                <img src="/images/shape4.png" width="60" height="60" alt="">
+                                <span>Antuan</span>
+                            </a>
+                            <a href="#">
+                                <img src="/images/shape4.png" width="60" height="60" alt="">
+                                <span>Milla</span>
+                            </a>
+                            <a href="#">
+                                <img src="/images/shape4.png" width="60" height="60" alt="">
+                                <span>Justine</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div class="footer-bottom">
+                <div class="social">
+                    <a href="#" class="btn btn-social btn-facebook"><i class="icon-social-facebook"></i></a>
+                    <a href="#" class="btn btn-social btn-twitter"><i class="icon-social-twitter"></i></a>
+                    <a href="#" class="btn btn-social btn-rss"><i class="icon-social-rss"></i></a>
+                    <a href="#" class="btn btn-social btn-delicious"><i class="icon-social-delicious"></i></a>
+                    <a href="#" class="btn btn-social btn-linkedin"><i class="icon-social-linkedin"></i></a>
+                    <a href="#" class="btn btn-social btn-flickr"><i class="icon-social-flickr"></i></a>
+                    <a href="#" class="btn btn-social btn-skype"><i class="icon-social-skype"></i></a>
+                    <a href="#" class="btn btn-social btn-mail"><i class="icon-social-mail"></i></a>
+                </div>
+                <span>© 2012 Powered by WooCommerce™. All Rights Reserved.</span>
+            </div>
+        </footer>
     </body>
 </html>
