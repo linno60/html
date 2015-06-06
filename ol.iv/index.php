92d3f2a4159498029773ae4290d7dc8783832f3b
Ivan <auzp@ukr.net>
2015-06-06 12:09:46 +0300

    commit

---
+++ ol.iv/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    function require_tpl($tpl) { require_once $_SERVER['DOCUMENT_ROOT'].'/tpls/'.$tpl.'.php'; }
+    $page = $_SERVER['QUERY_STRING'] ? substr($_SERVER['QUERY_STRING'], 1) : 'index';
+
+    switch ($page) 
+    {
+        case 'home': $layout = 'home'; break;
+    }
+
+    require_tpl($layout);
---
+++ ol.iv/tpls/home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Onlessons</title>
+        <meta name="HandheldFriendly" content="True">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">       
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <header>
+            <div class="header">
+                <a href="#" class="logo">
+                    <strong><span>on</span>Lessons.com</strong>
+                    <small>learn. teach. <span>anything</span>&trade;</small>
+                </a>
+                <form action="#" method="post">
+                    <div class="input-wrap">
+                        <i class="icon-username"></i>
+                        <input type="text" placeholder="username">
+                    </div>
+                    <div class="input-wrap">
+                        <i class="icon-password"></i>
+                        <input type="password" placeholder="password">
+                    </div>
+                    <button><span class="btn btn-login">Login</span></button>
+                </form>
+            </div>
+        </header>
+    </body>
+</html>
