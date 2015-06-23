192ea607d8ce7a95a93cf0947b320a8a1a548a4f
Ivan <auzp@ukr.net>
2015-06-23 21:07:31 +0300

    commit

---
+++ hubkick1.iv/tpls/home.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <meta name="HandheldFriendly" content="True">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link href='http://fonts.googleapis.com/css?family=Nothing+You+Could+Do' rel='stylesheet' type='text/css'>
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <header>
+            <div class="header">
+                <a href="#"><img src="images/logo.png"></a>
+                <nav>
+                    <a href="#" class="active">Home</a>
+                    <a href="#">Benefits</a>
+                    <a href="#">Pricing</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Register</a>
+                    <a href="#" class="sign">Sign in</a>
+                </nav>
+            </div>            
+        </header>
+        <div class="top-info-wrap">
+            <div class="top-info">
+                <h1>Not all email is created equal.</h1>
+                <p>HubKick is a <a href="#">free online tool</a> that let's you add an <a href="mailto:@hubkick.com">@hubkick.com</a> email address to your important emails so you can manage 
+                    <a href="#">what's important</a> like due dates, files, conversation, and the people you're working with.</p>
+                <div class='account-wrap'>
+                    <div></div>
+                    <form></form>
+                </div>
+                <div class='figure-wrap'>
+                    <figure>
+                        <div>
+                            <div class="figure-wrapper">
+                                <img src="images/img1.png">
+                            </div>
+                        </div>
+                        <figcaption>
+                            <h2>Hi I'm Joni and I create custom greeting cards.</h2>
+                        <p>I can't live without HubKick.  I organize client emails by forwarding them to an <a href="mailto:@hubkick.com">@hubkick.com</a> email address I create.  I then receive regular updates and summaries, so I never lose track of a client request again. "</p>                    
+                        </figcaption>
+                        </figure>
+                    <figure>
+                        <div>
+                            <div class="figure-wrapper">
+                                <img src="images/img2.png">
+                            </div>
+                        </div>
+                        <figcaption>
+                            <h2>Hey I'm Rich, I'm a freelance graphic designer.</h2>
+                        <p>The ability to get design files synced to my DropBox account is key.  No more digging through my inbox trying to figure out the latest revision and who sent it  Love you HubKick"</p>                    
+                        </figcaption></figure>
+                    <figure>
+                        <div>
+                            <div class="figure-wrapper">
+                                <img src="images/img3.png">
+                            </div>
+                        </div>
+                        <figcaption>
+                            <h2>My name is Ryan, and I make movies.</h2>
+                        <p>"I use HubKick for it’s quick ability to use email to tell my team what needs to get done, have it tracked, and know who is getting it done and who isn’t."</p>                    
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
