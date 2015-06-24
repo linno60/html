159c6d612a5f8186d131a1103b4f0d48ccaa9512
Ivan <auzp@ukr.net>
2015-06-24 09:11:41 +0300

    commit

--- hubkick1.iv/tpls/home.php
+++ hubkick1.iv/tpls/home.php
@@ -29,45 +29,51 @@
                 <p>HubKick is a <a href="#">free online tool</a> that let's you add an <a href="mailto:@hubkick.com">@hubkick.com</a> email address to your important emails so you can manage 
                     <a href="#">what's important</a> like due dates, files, conversation, and the people you're working with.</p>
                 <div class='account-wrap'>
-                    <div></div>
+                    <div>
+                        <p>Create your account now. Simply email: <a href="mailto:kick@hubkick.com">kick@hubkick.com</a>kick@hubkick.com</p>
+                    </div>
                     <form></form>
+                </div>               
+            </div>           
+        </div>
+        <div class='figure-wrap'>
+            <figure>
+                <div>
+                    <div class="figure-wrapper">
+                        <img src="images/img1.png">
+                    </div>
                 </div>
-                <div class='figure-wrap'>
-                    <figure>
-                        <div>
-                            <div class="figure-wrapper">
-                                <img src="images/img1.png">
-                            </div>
-                        </div>
-                        <figcaption>
-                            <h2>Hi I'm Joni and I create custom greeting cards.</h2>
-                        <p>I can't live without HubKick.  I organize client emails by forwarding them to an <a href="mailto:@hubkick.com">@hubkick.com</a> email address I create.  I then receive regular updates and summaries, so I never lose track of a client request again. "</p>                    
-                        </figcaption>
-                        </figure>
-                    <figure>
-                        <div>
-                            <div class="figure-wrapper">
-                                <img src="images/img2.png">
-                            </div>
-                        </div>
-                        <figcaption>
-                            <h2>Hey I'm Rich, I'm a freelance graphic designer.</h2>
-                        <p>The ability to get design files synced to my DropBox account is key.  No more digging through my inbox trying to figure out the latest revision and who sent it  Love you HubKick"</p>                    
-                        </figcaption></figure>
-                    <figure>
-                        <div>
-                            <div class="figure-wrapper">
-                                <img src="images/img3.png">
-                            </div>
-                        </div>
-                        <figcaption>
-                            <h2>My name is Ryan, and I make movies.</h2>
-                        <p>"I use HubKick for it’s quick ability to use email to tell my team what needs to get done, have it tracked, and know who is getting it done and who isn’t."</p>                    
-                        </figcaption>
-                    </figure>
+                <figcaption>
+                    <h2>Hi I'm Joni and I create custom greeting cards.</h2>
+                    <p>I can't live without HubKick.  I organize client emails by forwarding them to an <a href="mailto:@hubkick.com">@hubkick.com</a> email address I create.  I then receive regular updates and summaries, so I never lose track of a client request again. "</p>                    
+                </figcaption>
+            </figure>
+            <figure>
+                <div>
+                    <div class="figure-wrapper">
+                        <img src="images/img2.png">
+                    </div>
                 </div>
+                <figcaption>
+                    <h2>Hey I'm Rich, I'm a freelance graphic designer.</h2>
+                    <p>The ability to get design files synced to my DropBox account is key.  No more digging through my inbox trying to figure out the latest revision and who sent it  Love you HubKick"</p>                    
+                </figcaption></figure>
+            <figure>
+                <div>
+                    <div class="figure-wrapper">
+                        <img src="images/img3.png">
+                    </div>
+                </div>
+                <figcaption>
+                    <h2>My name is Ryan, and I make movies.</h2>
+                    <p>"I use HubKick for it’s quick ability to use email to tell my team what needs to get done, have it tracked, and know who is getting it done and who isn’t."</p>                    
+                </figcaption>
+            </figure>
+        </div>
+        <div class="reasons-wrap">
+            <div class="reasons">
+                <h1>there are so many reasons you're going to love hubkick.</h1>
             </div>
-
         </div>
     </body>
 </html>
