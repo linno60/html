b8a1c82722dd33f8ea0a36627b5bc97f918fe488
Ivan <auzp@ukr.net>
2015-07-02 18:17:34 +0300

    commit

--- sdrw.iv/tpls/home.php
+++ sdrw.iv/tpls/home.php
@@ -31,9 +31,61 @@
             <div class="map-wrap">
                 
             </div>
-            <div class="content">
-                
+            <div class="content-wrap">
+                <div class="content">
+                    <article>
+                        <h2>what to expect</h2>
+                    <p>Experience cuisine that delights your palate and defines the art of dining in San Diego. Held twice annually, this beloved culinary tradition features discounted prix fixe menus from more than 180 of San Diego’s best restaurants in 12 unique regions throughout San Diego County.</p>
+                    <p>No tickets or passes are required. Food lovers may simply dine out at as many participating restaurants as they like during San Diego Restaurant Week.</p>                
+                    </article>
+                    </div>
+                <aside>
+                    <iframe src="//www.youtube.com/embed/VIPdDb1Z_ck" frameborder="0" allowfullscreen></iframe>
+                </aside>
             </div>
         </header>
+        <section class="sponsors-wrap">
+            <h1>thanks to our sponsors</h1>
+            <img src="images/sp1.png">
+            <img src="images/sp2.png">
+            <img src="images/sp3.png">
+            <img src="images/sp4.png">
+            <img src="images/sp5.png">
+            <img src="images/sp6.png">
+            <img src="images/sp7.png">
+            <img src="images/sp8.png">
+            <img src="images/sp9.png">
+            <img src="images/sp10.png">
+            <img src="images/sp11.png">
+            <img src="images/sp12.png">
+            <img src="images/sp13.png">
+            <img src="images/sp14.png">
+            <img src="images/sp15.png">
+            <img src="images/sp16.png">
+            <img src="images/sp17.png">
+            <img src="images/sp18.png">
+            <img src="images/sp19.png">
+            <img src="images/sp20.png">
+            <img src="images/sp21.png">
+            <img src="images/sp22.png">
+            <img src="images/sp23.png">
+            <img src="images/sp24.png">
+            <img src="images/sp25.png">
+        </section>
+        <footer>
+            <div class="footer">
+                <ul>
+                    <li><a href="#">restaurants</a></li>
+                    <li><a href="#">california restaurant association</a></li>
+                    <li><a href="#">press</a></li>
+                    <li><a href="#">sponsors</a></li>
+                    <li><a href="#">get social</a></li>
+                    <li><a href="#">charity</a></li>
+                    <li><a href="#">contact</a></li>
+                    <li><a href="#">contest</a></li>
+                </ul>
+                <span>©2012 San Diego restaurant week, all rights reserved</span>
+            </div>
+        </footer>
     </body>
 </html>
