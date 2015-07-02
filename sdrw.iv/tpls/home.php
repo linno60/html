50d9660328e542dce91b9021ee11c26f629891e0
Ivan <auzp@ukr.net>
2015-07-02 16:45:08 +0300

    commit

---
+++ sdrw.iv/tpls/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <meta name="HandheldFriendly" content="True">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="nav-wrap">
+                <div class="nav">
+                    <a href="#">restaurants</a>
+                    <a href="#">california restaurant association</a>
+                    <a href="#">press</a>
+                    <a href="#">sponsors</a>
+                    <a href="#">get social</a>
+                    <a href="#">charity</a>
+                    <a href="#">contact</a>
+                    <a href="#">contest</a>
+                </div>
+                </div>
+            </nav>
+            <div class="banners">
+                <div class="banner-main"></div> 
+                <div class="banner-add"></div>
+            </div>
+            <div class="map-wrap">
+                
+            </div>
+            <div class="content">
+                
+            </div>
+        </header>
+    </body>
+</html>
