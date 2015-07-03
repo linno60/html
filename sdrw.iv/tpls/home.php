7b46838249840ff780238de518d524686d367af1
Ivan <auzp@ukr.net>
2015-07-03 20:03:26 +0300

    sdrw

--- sdrw.iv/tpls/home.php
+++ sdrw.iv/tpls/home.php
@@ -9,36 +9,115 @@
         <link type="text/css" rel="stylesheet" href="/css/styles.css">    
     </head>
     <body>
+        <div class="search-bar-wrap">
+            <div class="search-bar">
+                <h1>search</h1>
+                <form action="#" method="post">
+                    <div class="select-wrap">
+                        <label>By Cuisine</label>
+                        <span class="select">
+                            <select>
+                                <option selected>Choose one</option>
+                                <option>Italian</option>
+                                <option>Indian</option>
+                                <option>French</option>
+                                <option>Japanese</option>           
+                            </select>
+                        </span>
+                    </div>
+                    <div class="select-wrap">
+                        <label>By Neighborhood</label>
+                        <span class="select">
+                            <select>
+                                <option selected>Choose one</option>
+                                <option>Italian</option>
+                                <option>Indian</option>
+                                <option>French</option>
+                                <option>Japanese</option>           
+                            </select>
+                        </span>
+                    </div>
+                    <div class="select-wrap">
+                        <label>By Price</label>
+                        <span class="select">
+                            <select>
+                                <option selected>Choose one</option>
+                                <option>Italian</option>
+                                <option>Indian</option>
+                                <option>French</option>
+                                <option>Japanese</option>           
+                            </select>
+                        </span>
+                    </div>
+                    <div class="select-wrap">
+                        <label>By Restaurant</label>
+                        <input type="text" placeholder="Enter Restaurant Name">
+                    </div>
+                    <button><span class="btn btn-search"><i></i>Search</span></button>
+                </form>
+            </div>           
+        </div>
         <header>
             <nav>
-                <div class="nav-wrap">
-                <div class="nav">
-                    <a href="#">restaurants</a>
-                    <a href="#">california restaurant association</a>
-                    <a href="#">press</a>
-                    <a href="#">sponsors</a>
-                    <a href="#">get social</a>
-                    <a href="#">charity</a>
-                    <a href="#">contact</a>
-                    <a href="#">contest</a>
-                </div>
+                <div class="nav-wrap"> 
+                    <img class="banner-main" src="images/banner1.png">
+                    
+                    <div class="nav">
+                        <a href="#">restaurants</a>
+                        <a href="#">california restaurant association</a>
+                        <a href="#">press</a>
+                        <a href="#">sponsors</a>
+                        <a href="#">get social</a>
+                        <a href="#">charity</a>
+                        <a href="#">contact</a>
+                        <a href="#">contest</a>
+                    </div>
                 </div>
             </nav>
-            <div class="banners">
-                <div class="banner-main"></div> 
+            <div class="banners"> 
                 <div class="banner-add"></div>
             </div>
             <div class="map-wrap">
-                
+                <div class="map">
+                    <h1>6 days <br>over 180 restaurants</h1>
+                    <h2>where to start?</h2>
+                    <p>Choose a region and start exploring...this is just placeholer copy lorem ipsum dolor sit amet consectecur a non lorem! </p>
+                </div>
             </div>
             <div class="content-wrap">
                 <div class="content">
                     <article>
                         <h2>what to expect</h2>
-                    <p>Experience cuisine that delights your palate and defines the art of dining in San Diego. Held twice annually, this beloved culinary tradition features discounted prix fixe menus from more than 180 of San Diego’s best restaurants in 12 unique regions throughout San Diego County.</p>
-                    <p>No tickets or passes are required. Food lovers may simply dine out at as many participating restaurants as they like during San Diego Restaurant Week.</p>                
+                        <p>Experience cuisine that delights your palate and defines the art of dining in San Diego. Held twice annually, this beloved culinary tradition features discounted prix fixe menus from more than 180 of San Diego's best restaurants in 12 unique regions throughout San Diego County.</p>
+                        <p>No tickets or passes are required. Food lovers may simply dine out at as many participating restaurants as they like during San Diego Restaurant Week.</p>                
                     </article>
+                    <div class="menu-wrap">
+                        <div class="menu">
+                            <div class="hdr">
+                                <h3>lunch</h3>
+                                <a href="#"><i></i>See Lunch Menus</a>
+                            </div>
+                            <span>two-course $10, $15, $20</span>
+                        </div>
+                        <div class="menu">
+                            <div class="hdr">
+                                <h3>dinner</h3>
+                                <a href="#"><i></i>See Dinner Menus</a>
+                            </div>
+                            <span>three-course $20, $30, $40</span>
+                        </div>
                     </div>
+                    <div class="gallery">
+                        <div class="hdr">
+                            <h3>photo gallery</h3>
+                            <a href="#"><i></i>See More</a>
+                        </div>
+                        <div>
+                            <div class="btn"></div>
+                            <div class="btn"></div>
+                        </div>
+                    </div>
+                </div>
                 <aside>
                     <iframe src="//www.youtube.com/embed/VIPdDb1Z_ck" frameborder="0" allowfullscreen></iframe>
                     <img src="images/banner-side.png">
@@ -55,13 +134,87 @@
             <div class="facebook">
                 <div class="hdr">
                     <h1><img src="images/facebook.png">get social</h1>
-                    <a href="#">like</a>
+                    <a href="#"><i></i>Like</a>
+                </div>
+                <div class='expand'>
+                    <span>33 comments<i></i></span>
+                    <span class='add'>Add a comment</span>
+                </div>
+                <ul>
+                    <li>
+                        <div>
+                        <img src="images/1.png">
+                        <div>
+                            <a href='#' class='author'>Bianca Jackson</a><span>&middot;Argosy Univercity Hawaii</span>
+                            <p>Kitchen 1540! can't wait for SD Restaurant Week 2012.</p>
+                            <div class='links'>
+                                <a href='#'>Reply</a>&middot;
+                                <a href='#'><i></i>4</a>&middot;
+                                <a href='#'>Like</a>&middot;
+                                <span>December 21, 2011 at 3:33pm</span>
+                            </div>
+                        </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                        <img src="images/2.png">
+                        <div>
+                            <a href='#' class='author'>BreAnne Greig Custodio</a>
+                            <p>Yay! Thanks for the extension, I'm still full from two nights ago. I'll be ready for at least two more binges by next week</p>
+                            <div class='links'>
+                                <a href='#'>Reply</a>&middot;
+                                <a href='#'><i></i>1</a>&middot;
+                                <a href='#'>Like</a>&middot;
+                                <span>January 20 at 5:14pm</span>
+                            </div>
+                        </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                        <img src="images/3.png">
+                        <div>
+                            <a href='#' class='author'>Chello C Abutan</a><span>&middot;Chula Vista, California</span>
+                            <p>yay! extended another week,ok im hungry now</p>
+                            <div class='links'>
+                                <a href='#'>Reply</a>&middot;
+                                <a href='#'><i></i>1</a>&middot;
+                                <a href='#'>Like</a>&middot;
+                                <span>January 20 at 12:20pm</span>
+                            </div>
+                        </div>
+                        </div>
+                        <ul>
+                            <li>
+                                <div>
+                                <img src="images/4.png">
+                                <div>
+                                    <a href='#' class='author'>Aaron Minsch</a><span>&middot;Works at Jack Henry & Associates</span>
+                                    <p>enjoy life - eat out more often</p>
+                                    <div class='links'>
+                                        <a href='#'>Reply</a>&middot;
+                                        <a href='#'><i></i>1</a>&middot;
+                                        <a href='#'>Like</a>&middot;
+                                        <span>January 20 at 12:38pm</span>
+                                    </div>
+                                </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class='more'>
+                    <span>View 28 more<i></i></span>
+                </div>
+                <div class='facebook-footer'>
+                    <i></i>Facebook social plugin
                 </div>
             </div>
             <div class="twitter">
                 <div class="hdr">
                     <h1><img src="images/twitter.png">sdrw on twitter</h1>
-                    <span>Hashtag<a href="#">#sdrw</a></span>
+                    <span>Hashtag <a href="#">#sdrw</a></span>
                 </div>
                 <ul>
                     <li>
@@ -137,6 +290,13 @@
             <img src="images/sp23.png">
             <img src="images/sp24.png">
             <img src="images/sp25.png">
+            <img src="images/sp26.png">
+            <img src="images/sp27.png">
+            <img src="images/sp28.png">
+            <img src="images/sp29.png">
+            <img src="images/sp30.png">
+            <span>MasterCard and the MasterCard Brand Mark are registered trademarks of MasterCard International Incorporated. &copy;2011 MasterCard</span>
+
         </section>
         <footer>
             <div class="footer">
@@ -150,7 +310,7 @@
                     <li><a href="#">contact</a></li>
                     <li><a href="#">contest</a></li>
                 </ul>
-                <span>©2012 San Diego restaurant week, all rights reserved</span>
+                <span>&copy;2012 San Diego restaurant week, all rights reserved</span>
             </div>
         </footer>
     </body>
