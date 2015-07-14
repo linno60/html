bcc445b0ad5847cada8cdd8fa0673fc42580f11d
Ivan <auzp@ukr.net>
2015-07-14 10:47:49 +0300

    commit

---
+++ portfolio.iv/tpls/home.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Portfolio</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <header data-flex='row-center-center'>
+            <div data-flex='col-center-center'>
+                <i class="icon-logo"></i>
+                <h1>working<br> with design lab</h1>
+                <p>we've been helping startups launch and grow their businesses. we help you to fill the gap between development, design and product management.</p>
+            </div>
+        </header>
+        <section class='about-wrap' data-flex='col-center-center'>
+            <div class='about' data-flex='col-center-center'>
+                <h2>about us</h2>
+                <p>When you work with, you are part of a team fo committed collaborators. All aspects of our software development process are integrated, iterative and agile.</p>
+                <p>Our apprach unifies design, development and product management to create exceptional products. We start each project by understanding your business goals, the user's needs and the product requirenments. Then we help translate them into effective and elegant solutions.</p>
+            </div>
+        </section>
+        <section class='services-wrap' data-flex='col-center-center'>
+            <div class='services' data-flex='col-center-center'>
+                <h2>services</h2>
+                <p>before we sign a contract or write a line of code our team will spend the necessary time needed to understand your product vision. The outcome of theis scoping session is a proposal or hight-level statement  or work. Based on your requirements, we can provide a complete offering or just the pieces you need.</p>        
+            </div>
+        </section>
+        <section class='direction-wrap' data-flex='col-center-center'>
+            <div class='direction' data-flex='col-center-between'>
+                <h2>what&nbsp;we&nbsp;do</h2>
+                <article>
+                    <i class='icon-design'></i>
+                    <h3>design</h3>
+                    <p>Our design pracitce offers a full range of services including branding strategy, interaction and visual design and user experience testing. Throughout your project, our designers create and implement visual designs and workflows, solicit user feedback, and work with you to make sure what gets built is what is needed.</p>
+                </article>
+                <article>
+                    <i class='icon-develope'></i>
+                    <h3>development</h3>
+                    <p>All engineers are fluent in the latest enterprise, mobile and web deveopment technologies. They collaborate with your team to write, test, and improve code on a daily basis, using proven practices such as test-driven development and pair programing.</p>
+                </article>
+                <article>
+                    <i class='icon-product'></i>
+                    <h3>product management</h3>
+                    <p>Planning and development is iterative. Because we are constantly coding and testing, the products we build are always ready to go live. This iterative process allows for changes as business requirements evolve.</p>
+                </article>
+            </div>
+        </section>
+        <section class='works'>            
+            <div data-flex='row'>
+                <a href='#' data-flex='row'>
+                    <img src="images/img3.png">
+                    <span>topworkz</span>
+                </a>
+                <a href='#' data-flex='row'>
+                    <img src="images/img4.png">
+                    <span>joint</span>
+                </a>
+                <a href='#' data-flex='row'>
+                    <img src="images/img5.png">
+                    <span>market</span>
+                </a>
+            </div>
+            <div data-flex='row'>
+                <a href='#' data-flex='row'>
+                    <img src="images/img6.png">
+                    <span>worktop</span>
+                </a>
+                <a href='#' data-flex='row'>
+                    <img src="images/img7.png">
+                    <span>lable</span>
+                </a>
+                <a href='#' data-flex='row'>
+                    <img src="images/img8.png">
+                    <span>kyiv</span>
+                </a>
+            </div>
+        </section>
+        <section class='contacts-wrap' data-flex='col-center-center'>
+            <div class='contacts' data-flex='col-center-center'>
+                <h2>contact us</h2>
+                <p>Planning and development is iterative. Because we are constantly coding and testing, the produsts we build are always ready to go live. This iterative process allows for changes as business requirements evolve.</p>
+                <a href='#' class='btn btn-contact'>contact us</a>
+            </div>
+        </section>
+        <footer data-flex='row-center-center'>
+            <i class='icon-logo-alt'></i>
+            <span>&copy; 2014. All Rights Reserved Ellipsis.com</span>
+        </footer>
+    </body>
+</html>
