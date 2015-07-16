a462a2bdfb6c0cf176c3ccb24a4c0e753e1cfe0f
Ivan <auzp@ukr.net>
2015-07-16 11:03:52 +0300

    commit

---
+++ retro.iv/tpls/home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Retro</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <header>
+            <div class="header">
+                <nav>
+                <a href="#">about</a>
+                <a href="#">portfolio</a>
+                <a href="#" class="logo"></a>
+                <a href="#">my blog</a>
+                <a href="#">contact me</a>
+            </nav>
+            </div>
+            <div>
+                <h1>retro</h1>
+            </div>
+        </header>
+        <section class='about-wrap'>
+            
+        </section>
+        <section class='portfolio-wrap'>
+            
+        </section>
+        <section class='blog-wrap'>
+            
+        </section>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
