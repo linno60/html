88f17a2068c7e812cb40fc2b42878bd506f892c3
Ivan <auzp@ukr.net>
2015-07-29 22:27:11 +0300

    commit

---
+++ lipsum.iv/tpls/home.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Lipsum</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <div class="wrap">
+            <header>
+                <div class="header" data-flex="row-center">
+                    <a href="#" class="logo" data-flex="row-center"><img src="images/logo.png">Lipsum</a>
+                    <div data-flex="col--center">
+                        <h2>lorem ipsum dolor sit amet</h2>
+                        <span>consectetur adipisicing elit, sed do eiusmod tempor incididunt</span>
+                    </div>
+                </div>
+                <nav class="nav" data-flex="row-center">
+                    <a href="#">home</a>
+                    <a href="#">products</a>
+                    <a href="#">news</a>
+                    <a href="#">about us</a>
+                    <a href="#">contact us</a>
+                </nav>
+            </header>
+            <section class="banners">
+                <h2>featured products</h2>
+                <div data-flex="row-start-between">
+                    <div class="figure">
+                        <figure data-flex>
+                            <img src="images/img1.png">
+                            <div class="caption" data-flex="row-center">
+                                <a href="#" class="btn btn-left"><i class="icon-left"></i></a>
+                                <a href="#">Lorem comoodo Manager</a>
+                                <a href="#" class="btn btn-right"><i class="icon-right"></i></a>    
+                            </div>
+                        </figure>
+                    </div>
+                    <div class="preview">
+                        <figure data-flex>
+                            <img src="images/img2.png">
+                            <figcaption data-flex="col">
+                                <small>preview release available!</small>
+                                <span>lispum data comoodo</span>
+                            </figcaption>
+                        </figure>
+                        <p>The mission of Orlin Research, Inc. is to produce software products that multiply the power and efficiency of empirical scientific research in the social sciences.</p>
+                        <p> A central goal is to expand access to complex data sources and to the advanced methods of scientific investigation needed to analyze them.</p>
+                        <div data-flex="row--end">
+                        <a href="#" class="btn btn-more"><em>More</em><i class="icon-arrow"></i></a>
+                    </div>  
+                    </div>
+                </div>
+            </section>
+            <section class="content" data-flex="row--between">
+                <div class="news">
+                    <h4>latest news</h4>
+                    <article data-flex="row-start">
+                        <div class="date" data-flex="col">
+                            <span>27</span>
+                            <small>nov</small>
+                        </div>
+                        <div>
+                            <h5><a href="#">An early access preview</a></h5>
+                            <p>An early access preview of the Orlin Data Manager is available...</p>
+                        </div>
+                    </article>
+                    <article data-flex="row-start">
+                        <div class="date" data-flex="col">
+                            <span>27</span>
+                            <small>nov</small>
+                        </div>
+                        <div>
+                            <h5><a href="#">An early access preview</a></h5>
+                            <p>An early access preview of the Orlin Data Manager is available...</p>
+                        </div>
+                    </article>
+                    <article data-flex="row-start">
+                        <div class="date" data-flex="col">
+                            <span>27</span>
+                            <small>nov</small>
+                        </div>
+                        <div>
+                            <h5><a href="#">An early access preview</a></h5>
+                            <p>An early access preview of the Orlin Data Manager is available...</p>
+                        </div>
+                    </article>
+                </div>
+                <div class="description">
+                    <h4>who we are ?</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <div data-flex="row--end">
+                        <a href="#" class="btn btn-more"><em>More</em><i class="icon-arrow"></i></a>
+                    </div>    
+                </div>
+                <div class="contact">
+                    <h4>contuct us</h4>
+                    <div data-flex="row-center">
+                        <i class="icon-phone"></i>
+                        <span>(626) 921-4592</span>
+                    </div>
+                    <div data-flex="row-center">
+                        <i class="icon-fax"></i>
+                        <span>(626) 921-4592</span>
+                    </div>
+                    <div data-flex="row-center">
+                        <i class="icon-mail"></i>
+                        <a href="mailto:info@orlinresearch.com">info@orlinresearch.com</a>
+                    </div>
+                    <div data-flex="row-start">
+                        <i class="icon-address"></i>
+                        <address data-flex="col">
+                            <span>Orlin Research, Inc.</span>
+                            <span>7412 Ridgewood Ave</span>
+                            <span>Chevy Chase, MD 20815</span>
+                            <span>USA</span>
+                        </address>
+                    </div>
+                </div>
+            </section>
+            <section class="clients" data-flex="row-center-around">
+                <h3>our clients</h3>
+                <img src="images/at.png">
+                <img src="images/inc.png">
+                <img src="images/autoimunne.png">
+                <img src="images/visa.png">
+                <img src="images/polar.png">
+                <img src="images/gap.png">
+            </section>
+            <footer data-flex="col-center-center">
+                <a href="#" class="logo" data-flex="row-center"><img src="images/logo-alt.png">Lipsum</a>
+                <nav class="nav-footer" data-flex="row-center-center">
+                    <a href="#">home</a>
+                    <a href="#">about us</a>
+                    <a href="#">work</a>
+                    <a href="#">services</a>
+                    <a href="#">resourses</a>
+                    <a href="#">articles</a>
+                    <a href="#">contact</a>
+                </nav>
+                <span class="copyright">© 2009 Orlin Research, Inc </span>
+            </footer>
+        </div>
+    </body>
+</html>
---
+++ pulse.iv/tpls/home.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Pulse</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>        
+        <header>
+            <div class="header" data-flex="row-center-between">
+                <a href="#" class="logo">Pulse&nbsp;*</a>
+                <nav class="nav" data-flex="row-center">
+                    <a href="#" class="active">home</a>
+                    <a href="#">services</a>
+                    <a href="#">portfolio</a>
+                    <a href="#">products</a>
+                    <a href="#">blog</a>
+                    <a href="#">contact us</a>
+                </nav>
+            </div> 
+            <div class="slider" data-flex>
+                <div>
+                    <img src="images/img1.png">
+                     <img src="images/img2.png" width="514" height="312">
+                      <img src="images/img3.png">
+                </div>
+                <div data-flex="col-start">
+                    <h1>Download Excellent PSD Template</h1>
+                    <p>GraphicsFuel.com is a design blog where you can download high quality “Photoshop Freebies” with the original source files in .PSD format.  Absolutely free!</p>
+                    <a href="#" class="btn btn-browse"><em>Browse Template</em><span data-flex="row-center-center"><i class="icon-browse"></i></span></a>
+                    <div class="controls" data-flex>
+                        <a href="#"></a>
+                        <a href="#" class="active"></a>
+                        <a href="#"></a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section class="about-wrap">
+            <div class="about" data-flex="row-center">
+                <img src="images/case.png">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id accumsan massa. Donec suscipit commodo velit. Fusce pharetra mi sit amet felis auctor tempor. Donec vel odio enim, id ultricies odio. Ut euismod arcu a tortor consectetur eu lobortis augue congue. Duis quis ipsum quam.</p>
+                <a href="#" class="btn btn-learn"><i class="icon-learn"></i><em>learn more</em></a>
+            </div>
+        </section>
+        <section class="content-wrap">
+            <div class="content">
+                <div class="grid" data-flex="row--around">
+                    <article>
+                        <h4>Service Text Heading #1</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id accumsan massa. Donec suscipit commodo velit. Fusce pharetra mi sit amet felis auctor tempor. Donec vel odio enim, id ultricies odio. </p>
+                        <a href="#" class="btn btn-default">More ::</a>
+                    </article>
+                    <article>
+                        <h4>Service Text Heading #1</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id accumsan massa. Donec suscipit commodo velit. Fusce pharetra mi sit amet felis auctor tempor. Donec vel odio enim, id ultricies odio. </p>
+                        <a href="#" class="btn btn-default">More ::</a>
+                    </article>
+                    <article>
+                        <h4>Service Text Heading #1</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id accumsan massa. Donec suscipit commodo velit. Fusce pharetra mi sit amet felis auctor tempor. Donec vel odio enim, id ultricies odio. </p>
+                        <a href="#" class="btn btn-default">More ::</a>
+                    </article>
+                </div>
+                <div class="slideshow">
+                    <a class="btn btn-slide left"><i class="icon-left"></i></a>
+                    <div class="grid" data-flex="row--between">
+                        <a href="#" data-flex="col">
+                            <img src="images/img4.png">
+                            <span data-flex="row-center">Ecommerce Icons<i class="icon-arrow"></i></span>
+                        </a>
+                        <a href="#" data-flex="col">
+                            <img src="images/img5.png">
+                            <span data-flex="row-center">Fruit Illustration<i class="icon-arrow"></i></span>
+                        </a>
+                        <a href="#" data-flex="col">
+                            <img src="images/img6.png">
+                            <span data-flex="row-center">Business Portfolio<i class="icon-arrow"></i></span>
+                        </a>
+                        <a href="#" data-flex="col">
+                            <img src="images/img7.png">
+                            <span data-flex="row-center">Pricing Table Design<i class="icon-arrow"></i></span>
+                        </a>
+                    </div>
+                    <a class="btn btn-slide right"><i class="icon-right"></i></a>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div class="footer" data-flex="row--between">
+                <div class="textual">
+                    <h5>Textual Content</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id accumsan massa. Donec suscipit commodo velit. Fusce pharetra mi sit amet felis auctor tempor. Donec vel odio enim, id ultricies odio. </p>
+                </div>
+                <div class="browse">
+                    <h5>Browse</h5>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                        <li>
+                            <a href="#">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#">Products</a>
+                        </li>
+                        <li>
+                            <a href="#">Blog</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="connect">
+                    <h5>Connect Us</h5>
+                    <div class="social" data-flex="row--between">
+                        <a href="#" class="btn btn-social"><i class="icon-tw"></i></a>
+                        <a href="#" class="btn btn-social"><i class="icon-in"></i></a>
+                        <a href="#" class="btn btn-social"><i class="icon-fb"></i></a>
+                        <a href="#" class="btn btn-social"><i class="icon-mail"></i></a>
+                    </div>
+                </div>
+                <div>
+                    <h5>Get In Touch With Us</h5>
+                    <form action="#" method="post" data-flex="col">
+                        <input type="text" placeholder="Your Name">
+                        <input type="text" placeholder="Your Email">
+                        <textarea type="text" placeholder="Your Message"></textarea>
+                    </form>
+                </div>
+            </div>
+            <div class="copyright" data-flex="row-center-center">
+                <span>&copy; 2011 Pulso by GraphicsFuel.com&nbsp;|&nbsp;<a href="#">Terms of service</a>&nbsp;|&nbsp;<a href="#">Privacy policy</a></span>
+            </div>
+        </footer>
+    </body>
+</html>
