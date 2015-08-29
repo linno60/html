a996bd1a3ec8d1dd397ec1ba4ad6447a66120423
Ivan <auzp@ukr.net>
2015-08-30 00:09:00 +0300

    commit

--- creativemass.iv/tpls/home.php
+++ creativemass.iv/tpls/home.php
@@ -10,32 +10,161 @@
     </head>
     <body>
         <div class="wrap">
-            <header>
+            <a href='#top' class='btn btn-default anchor'><i class='icon-arrow'></i></a>
+            <header data-flex="col-center">
+                <a name='top'></a>
                 <a href="#" class="logo" data-flex="col-center"><b>creativemass</b><span>core unstable be prepared</span></a>
                 <nav class="nav" data-flex="row-center-center">
                     <a href="#">work</a>
                     <a href="#">services</a>
+                    <img src="images/orbit.png">
                     <a href="#">about</a>
                     <a href="#">contact</a>
                 </nav>
             </header>
-            <section class="works"> 
-                <div data-flex="row-center-between">
-                    <h2>latest works</h2>
+            <section class="works-wrap"> 
+                <h2 data-flex="row-center-between">latest works
                     <div class="arrows">
-                        <a href="#"></a>
-                        <a href="#"></a>
+                        <a href="#" class="btn btn-slider prev"></a>
+                        <a href="#" class="btn btn-slider next"></a>
+                    </div>
+                </h2>                    
+                <div data-flex="row-start-between" class="works">
+                    <div data-flex>
+                        <figure data-flex>
+                            <img src="images/img1.png">
+                        </figure>
+                    </div>
+                    <div data-flex="row--between" class="figure-wrap">
+                        <figure data-flex>
+                            <img src="images/img2.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="images/img3.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="images/img4.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="images/img5.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="images/img6.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="images/img7.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="images/img8.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="images/img9.png">
+                        </figure>
                     </div>
                 </div>
             </section>
             <section class="content" data-flex="row--between">
                 <div class="services">
-                    
+                    <h2>services</h2>
+                    <article data-flex="row-start">
+                        <figure data-flex>
+                            <img src="images/img10.png">
+                        </figure>
+                        <div>
+                            <h3>animation</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>
+                        </div>
+                    </article>
+                    <article data-flex="row-start">
+                        <figure data-flex>
+                            <img src="images/img11.png">
+                        </figure>
+                        <div>
+                            <h3>filming and animation</h3>
+                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage.</p>
+                        </div>
+                    </article>
+                    <article data-flex="row-start">
+                        <figure data-flex>
+                            <img src="images/img12.png">
+                        </figure>
+                        <div>
+                            <h3>producing</h3>
+                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage.</p>
+                        </div>
+                    </article>
                 </div>
                 <aside class="aside">
-                    
+                    <h2>testimonials</h2>
+                    <blockquote>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur”</blockquote>
+                    <cite>John Doe  <a href="#">- InspiredCore.com</a></cite>
+                    <h2>awards</h2>
+                    <div data-flex>
+                        <i class="icon-oscar"></i>
+                        <span>Academy Award for Best Animated Short Film</span>
+                    </div>
+                    <h2>connect</h2>
+                    <div class="social" data-flex="row--between">
+                        <a href="#"><i class="icon-rss"></i></a>
+                        <a href="#"><i class="icon-tw"></i></a>
+                        <a href="#"><i class="icon-fb"></i></a>
+                        <a href="#"><i class="icon-in"></i></a>
+                        <a href="#"><i class="icon-sk"></i></a>
+                        <a href="#"><i class="icon-yt"></i></a>
+                    </div>
                 </aside>
             </section>
+            <section class="about">
+                <h2>about company</h2>
+                <div data-flex="row--between">
+                    <article data-flex="row-start">
+                        <figure data-flex>
+                            <img src="images/img13.png">
+                        </figure>
+                        <div>
+                            <h3>lorem ipsum dolor sit ametnulla</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.</p>
+                        </div>
+                    </article>
+                    <div class="clients">
+                        <h3>client list</h3>
+                        <div data-flex="row-center-between">
+                            <img src="images/toyota.png">
+                            <img src="images/coca.png">
+                            <img src="images/disney.png">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <footer class="footer">
+                <h2>contact iformation</h2>
+                <div data-flex="row--between">
+                    <form action="#" method="post" data-flex="row--between">
+                        <div data-flex="col-end">
+                            <input type="text" placeholder="Full name">
+                            <input type="text" placeholder="contact@inspiredcore.com">
+                            <input type="text" placeholder="Subject">
+                            <button><span class="btn btn-default">send</span></button>
+                        </div>
+                        <textarea placeholder="Message goes here"></textarea>
+                    </form>
+                    <div class="contacts">
+                        <span>John Doe</span>
+                        <address>66, Highway to hell, FL 00666</address>
+                        <div data-flex class="info">
+                            <div class="lbl lbl-default"><i class="icon-info"></i></div>
+                            <div data-flex="col">
+                                <a href="mailto:contact@inspiredcore.ocm">contact@inspiredcore.ocm</a>
+                                <span>+40(744)123xxx</span>
+                            </div>
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                    </div>
+                </div> 
+                <section class="copyright">
+                    <span>&copy; 2011 Your Company. All Rights Reserved</span>
+                </section>
+            </footer>
         </div>
     </body>
 </html>
