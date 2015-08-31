0e98c6e5a0554c4b24dd91b8d1f9e83a639185dd
Ivan <auzp@ukr.net>
2015-08-31 09:59:25 +0300

    commit

---
+++ news.iv/tpls/home.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>News</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <header>
+            <section class="top-bar-wrap">
+                <div class="top-bar" data-flex="row-center-between">
+                    <p>Having trouble viewing this email?<a href="#"> View it in your browser</a></p>
+                    <div data-flex>
+                        <a href="#"><i class="icon-arrow"></i><span>Online version</span></a>
+                        <a href="#"><i class="icon-arrow"></i><span>Forward to a Friend</span></a>
+                    </div>
+                </div>
+            </section>
+            <section class="header-wrap">
+                <div class="header" data-flex="row-center-between">
+                    <a href="#" class="logo" data-flex="row-center">
+                        <i class="icon-logo"></i>
+                        <h1 data-flex="col">
+                            <b>E-Mail Template</b>
+                            <small>Free Email Template</small>
+                        </h1>
+                    </a>
+                    <span>January 16</span>
+                </div>
+            </section>
+        </header>
+        <section class="featured-wrap">
+            <div class="featured" data-flex>
+                <div>
+                    <h1>Featured Title Goes Right Here Like This</h1>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+                    <div data-flex>
+                        <a href="#" class="btn btn-start">Get Started</a>
+                        <a href="#" class="btn btn-default">Online Demo</a>
+                    </div>
+                </div>
+                <div>
+                    <img src="images/img.png">
+                </div>
+            </div>
+        </section>
+        <section class="content-wrap" data-flex="row--between">
+            <div>
+                <h2>Heading Title Goes Right Here</h2>
+                <article data-flex="row--between">
+                    <div>
+                        <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+                        <p>Dor do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                        <a href="#"><i class="icon-list"></i><span>Read More</span></a>
+                    </div>
+                    <div>
+                        <img src="images/img.png">
+                    </div>
+                </article>
+            </div>
+            <aside>
+                <article>
+                   <h2>Sidebar List</h2>
+                <span></span> 
+                <ul>
+                    <li><a href="#">Lorem Ipsum dolor</a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                    <li><a href="#"></a></li>
+                </ul>
+                </article>
+            </aside>
+        </section>
+    </body>
+</html>
