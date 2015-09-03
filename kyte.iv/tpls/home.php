8d6a5c24009d0ce7c6b53279c8bb4a230e57c9ee
Ivan <auzp@ukr.net>
2015-09-03 09:57:16 +0300

    commit

---
+++ kyte.iv/tpls/home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Kyte</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link href='https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <header>
+            <section class="header" data-flex="row-center-between">
+                <a href="#" class="logo" data-flex='row-center'><i class="icon-logo"></i>kyte</a>
+                <nav class="nav">
+                    <a href="#" class='active'>home</a>
+                    <a href="#">about</a>
+                    <a href="#">portfolio</a>
+                    <a href="#">journal</a>
+                    <a href="#">features</a>
+                    <a href="#">contact</a>
+                </nav>
+            </section>
+        </header>
+        <section class="slider-wrap">
+            <div class="slider" data-flex="col-center-center">
+                <h1>i'm kyte broten</h1>
+                <span>freelance photographer</span>
+                <div class="social" data-flex>
+                    <a href="#"><i class="icon-twitter"></i></a>
+                    <a href="#"><i class="icon-instagram"></i></a>
+                    <a href="#"><i class="icon-dribble"></i></a>
+                    <a href="#"><i class="icon-mail"></i></a>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class='hdr' data-flex='row-center'>
+                <h2>a little about me</h2>
+            </div>
+        </section>
+    </body>
+</html>
--- news-3.iv/tpls/home.php
+++ news-3.iv/tpls/home.php
@@ -4,24 +4,24 @@
         <title>News-3</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-        <link type="text/css" rel="stylesheet" href="/css/reset.css">
-        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
-        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+        <link type="text/css" rel="stylesheet" href="css/reset.css">
+        <link type="text/css" rel="stylesheet" href="css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="css/styles.css">    
     </head>
     <body>
         <header>
             <div class="top-bar" data-flex="row-center-between">
-                <p>Having trouble reading this email? <a href="#">View it in your browser</a></p>
+                <p>Having trouble viewing this email? <a href="#">View it in your browser</a></p>
                 <div data-flex>
-                    <a href="#"><i class="icon-arrow"></i><span>Online version</span></a>
-                    <a href="#"><i class="icon-arrow"></i><span>Forward to a Friend</span></a>
+                    <a href="#"><i class="icon-arrow"></i><span>Online Version</span></a>
+                    <a href="#"><i class="icon-arrow"></i><span>Forward to a friend</span></a>
                 </div>
             </div>         
         </header>
         <div class="wrap">
-            <section data-flex="row--between">
+            <section class="logo-wrap" data-flex="row--between">
                 <a href="#" class="logo">
-                    <img src="/images/logo.png">
+                    <img src="images/logo.png">
                 </a>
                 <div data-flex="row-end">
                     <div class="date" data-flex="row-center-center">
@@ -37,107 +37,159 @@
                     <a href='#' class='btn btn-view'>View full Article</a>
                 </div>
             </section>
-            <section class="content-wrap" data-flex='row--between'>
-                <div class='content'>
-                    <div class='article-wrap'>
-                        <article>
-                            <h2>Healthy Meal Every Day</h2>
-                            <figure>
-                                <img src='/images/img3.png'>
-                            </figure>
-                            <p>Quisque ligula arcu, in rutrum magna. Ut varius imperdiet congue. Vivamus fringilla dignissim purus, quis vitae mollis urna tincidunt amet raesent vel odio at massa sagittis.</p>
-                            <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read more</a>
-                        </article>
-                        <div data-flex='row--between'>
+            <section class="content-wrap">
+                <div data-flex='row--between'>
+                    <div class='content'>
+                        <div class='article-wrap'>
                             <article>
-                                <figure>
-                                    <img src='/images/img4.png'>
-                                    <div class='title' data-flex="row-center-center">79 calories</div>
+                                <h2>Healthy Meal Every Day</h2>
+                                <figure data-flex>
+                                    <img src='images/img3.png'>
+                                    <figcaption class='title' data-flex="row-center-center">15 calories</figcaption>
                                 </figure>
-                                <h3>Lorem ipsum dolor sit adipising dor</h3>
-                                <p>Quisque vitae ligula arcu, in rutrum magna. Ut varius imperdiet congue vivamus.</p>
-                                <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read more</a>
+                                <p>Quisque ligula arcu, in rutrum magna. Ut varius imperdiet congue. Vivamus fringilla dignissim purus, quis <a href="#">vitae</a> mollis urna tincidunt amet raesent vel odio at massa sagittis.</p>
+                                <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read More</a>
                             </article>
-                            <article>
-                                <figure>
-                                    <img src='/images/img4.png'>
-                                    <div class='title' data-flex="row-center-center">26 calories</div>
+                            <div data-flex='row--between'>
+                                <article>
+                                    <figure data-flex>
+                                        <img src='images/img4.png'>
+                                        <figcaption class='title' data-flex="row-center-center">79 calories</figcaption>
+                                    </figure>
+                                    <h3>Lorem ipsum dolor sit adipiscing dor</h3>
+                                    <p>Quisque vitae ligula arcu, in rutrum magna. Ut varius imperdiet congue vivamus.</p>
+                                    <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read More</a>
+                                </article>
+                                <article>
+                                    <figure data-flex>
+                                        <img src='images/img4.png'>
+                                        <figcaption class='title' data-flex="row-center-center">26 calories</figcaption>
+                                    </figure>
+                                    <h3>Lipsum dolor sit dor amet adipiscing</h3>                                    
+                                    <p>Quisque vitae ligula arcu, in rutrum magna. Ut varius imperdiet congue vivamus.</p>
+                                    <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read More</a>
+                                </article>
+                            </div>
+                        </div>
+                        <div class="articles">
+                            <h2>Three Simple Vegetarian Recipes</h2>
+                            <article data-flex="row-start">
+                                <figure data-flex>
+                                    <img src='images/img6.png'>
+                                    <figcaption class='title' data-flex="row-center-center">40 calories</figcaption>
                                 </figure>
-                                <h3>Lorem ipsum dolor sit adipising dor</h3>
-                                <p>Quisque vitae ligula arcu, in rutrum magna. Ut varius imperdiet congue vivamus.</p>
-                                <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read more</a>
+                                <div>
+                                    <h3>Lipsum dolor sit dor amet adipiscing</h3>
+                                    <p>Quisque vitae ligula arcu, in rutrum magna. Ut varius imperdiet congue vivamus.</p>
+                                    <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read More</a>
+                                </div>
+                            </article>
+                            <article data-flex="row-start">
+                                <figure data-flex>
+                                    <img src='images/img6.png'>
+                                    <figcaption class='title' data-flex="row-center-center">96 calories</figcaption>
+                                </figure>
+                                <div>
+                                    <h3>Lipsum dolor sit dor amet adipiscing</h3>
+                                    <p>Quisque vitae ligula arcu, in rutrum magna. Ut varius imperdiet congue vivamus.</p>
+                                    <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read More</a>
+                                </div>
+                            </article>
+                            <article data-flex="row-start">
+                                <figure data-flex>
+                                    <img src='images/img6.png'>
+                                    <figcaption class='title' data-flex="row-center-center">34 calories</figcaption>
+                                </figure>
+                                <div>
+                                    <h3>Lipsum dolor sit dor amet adipiscing</h3>
+                                    <p>Quisque vitae ligula arcu, in rutrum magna. Ut varius imperdiet congue vivamus.</p>
+                                    <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read More</a>
+                                </div>
                             </article>
                         </div>
                     </div>
-                    <div>
-                        <article data-flex>
-                            <figure>
-                                <img src='/images/img4.png'>
-                                <div class='title' data-flex="row-center-center">40 calories</div>
+                    <aside class='aside'>
+                        <article class="recipe">
+                            <h2>Latest Recipe</h2>
+                            <figure data-flex>
+                                <img src='images/img7.png'>
+                                <figcaption class='title' data-flex="row-center-center">Crème brûlée</figcaption>
                             </figure>
-                            <div>
-                                <h3>Lorem ipsum dolor sit adipising dor</h3>
-                                <p>Quisque vitae ligula arcu, in rutrum magna. Ut varius imperdiet congue vivamus.</p>
-                                <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read more</a>
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <td>Eggs</td>
+                                        <td>6 pcs</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Milk</td>
+                                        <td>500 ml</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Sugar</td>
+                                        <td>20 g</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Flour</td>
+                                        <td>500 g</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </article>
+                        <article class="healthy">
+                            <h2>Healthy or Not?</h2>
+                            <figure data-flex="col-center-center">
+                                <img src="images/fish.png">
+                                <figcaption>Fried Fish</figcaption>
+                            </figure>
+                            <div class="compare" data-flex="row--between">
+                                <div data-flex="col-center">
+                                    <span data-flex="row-center"><i class="icon-like"></i>Yes</span>
+                                    <em>90%</em>
+                                </div>
+                                <div data-flex="col-center">
+                                    <span data-flex="row-center"><i class="icon-dislike"></i>No</span>
+                                    <em>10%</em>
+                                </div>
                             </div>
                         </article>
-                        <article data-flex>
-                            <figure>
-                                <img src='/images/img4.png'>
-                                <div class='title' data-flex="row-center-center">96 calories</div>
+                        <article class="week">
+                            <h2>Meal of the week</h2>
+                            <figure data-flex>
+                                <img src="images/img5.png">
+                                <figcaption class="title" data-flex="row-center-center">Ratatouille</figcaption>
                             </figure>
-                            <div>
-                                <h3>Lorem ipsum dolor sit adipising dor</h3>
-                                <p>Quisque vitae ligula arcu, in rutrum magna. Ut varius imperdiet congue vivamus.</p>
-                                <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read more</a>
+                        </article>
+                        <article class="partners">
+                            <h2>Our Partners</h2>
+                            <div data-flex="row--between">
+                                <img src="images/img1.png">
+                                <img src="images/img2.png">
                             </div>
                         </article>
-                        <article data-flex>
-                            <figure>
-                                <img src='/images/img4.png'>
-                                <div class='title' data-flex="row-center-center">34 calories</div>
-                            </figure>
-                            <div>
-                                <h3>Lorem ipsum dolor sit adipising dor</h3>
-                                <p>Quisque vitae ligula arcu, in rutrum magna. Ut varius imperdiet congue vivamus.</p>
-                                <a href='#' data-flex='row-center'><i class='icon-dot'></i>Read more</a>
-                            </div>
-                        </article>
-                    </div>
+                    </aside>
                 </div>
-                <aside class='aside'>
-                    <article>
-                        <h2>Latest Recipe</h2>
-                        <figure>
-                                <img src='/images/img4.png'>
-                                <div class='title' data-flex="row-center-center">96 calories</div>
-                            </figure>
-                        <table class="table">
-                            <tbody>
-                                <tr>
-                                    <td>Eggs</td>
-                                    <td>5 pcs</td>
-                                </tr>
-                                <tr>
-                                    <td>Milk</td>
-                                    <td>500 ml</td>
-                                </tr>
-                                <tr>
-                                    <td>Sugar</td>
-                                    <td>20 g</td>
-                                </tr>
-                                <tr>
-                                    <td>Flour</td>
-                                    <td>500 g</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </article>
-                    <article>
-                        <h2>Healthy or Not?</h2>
-                    </article>
-                </aside>
+                <section class="social" data-flex="row-center-around">
+                <a href="#" data-flex="row-center"><i class="icon-fb"></i>facebook</a>
+                <a href="#" data-flex="row-center"><i class="icon-tw"></i>twitter</a>
+                <a href="#" data-flex="row-center"><i class="icon-rss"></i>rss feed</a>
+                <a href="#" data-flex="row-center"><i class="icon-ms"></i>myspace</a>
+                <a href="#" data-flex="row-center"><i class="icon-cn"></i>cooknews</a>
             </section>
+            </section>            
         </div>
+        <footer class="footer" data-flex="col-center">
+            <nav data-flex="row-center-center">
+                <a href="#">Unsubscribe</a>|
+                <a href="#">Privacy Policy</a>|
+                <a href="#">Terms & Conditions</a>
+            </nav>
+            <span>Copyright © 2013 Company Name. All Rights Reserved. East Pixel Bld. 99, Creative City 9000, Republic of Design</span>
+            <div class="contacts" data-flex="row-center-center">
+                <a href="#" class="web">www.YourSiteName.com</a>|
+                <a href="mailto:email@yoursitename.com">email@yoursitename.com</a>
+                <span>Phone: +1 (655) 606-605</span>
+            </div>
+        </footer>
     </body>
 </html>
