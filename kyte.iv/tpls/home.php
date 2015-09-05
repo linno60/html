86326d9ac22f5790722a9f3ba001c0b3e690121f
Ivan <auzp@ukr.net>
2015-09-05 15:49:56 +0300

    commit

--- kyte.iv/tpls/home.php
+++ kyte.iv/tpls/home.php
@@ -4,7 +4,6 @@
         <title>Kyte</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-        <link href='https://fonts.googleapis.com/css?family=Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
         <link type="text/css" rel="stylesheet" href="/css/reset.css">
         <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
         <link type="text/css" rel="stylesheet" href="/css/styles.css">    
@@ -25,13 +24,13 @@
         </header>
         <section class="slider-wrap">
             <div class="slider" data-flex="col-center-center">
-                <h1>i'm kyte broten</h1>
-                <span>freelance photographer</span>
-                <div class="social" data-flex>
-                    <a href="#"><i class="icon-twitter"></i></a>
-                    <a href="#"><i class="icon-instagram"></i></a>
-                    <a href="#"><i class="icon-dribble"></i></a>
-                    <a href="#"><i class="icon-mail"></i></a>
+                <h1>i'm kyte brooten</h1>
+                <span>frelance photographer</span>
+                <div class="social" data-flex="row-center-center">
+                    <a href="#" class="btn btn-social"><i class="icon-twitter"></i></a>
+                    <a href="#" class="btn btn-social"><i class="icon-instagram"></i></a>
+                    <a href="#" class="btn btn-social"><i class="icon-dribble"></i></a>
+                    <a href="#" class="btn btn-social"><i class="icon-mail"></i></a>
                 </div>
             </div>
         </section>
@@ -39,6 +38,162 @@
             <div class='hdr' data-flex='row-center'>
                 <h2>a little about me</h2>
             </div>
+            <div class="about" data-flex="col-center">
+                <article data-flex="row-start">
+                    <figure data-flex>
+                        <img src="/images/circle.png">
+                    </figure>
+                    <div>
+                        <span>Vestibulum ligut praesent commodo cursus magna, scelerisque  sit consectetur et. Cum sociis natoque penatibus et magnis dis tellus parturient pharetra montes.</span>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas sed diam eget risus varius blandit sit amet non magna. Aenean lacinia bibendum nulla sed consectetur. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam id dolor id nibh ultricies vehicula ut id elit, fusce dapibus, tellus ac cursus commodo.</p>
+                    </div>
+                </article>
+                <div data-flex="row--between">
+                    <article>
+                        <h4>what i do</h4>
+                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.  Consectetur adipiscing vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum ullamcorper.</p>
+                    </article>
+                    <article>
+                        <h4>what my process</h4>
+                        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas faucibus mollis interdum. Nullam quis risus eget urna mollis ornare vel eu leo. Sed posuere consectetur est at lobortis. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    </article>
+                    <article>
+                        <h4>what my skills</h4>
+                        <p>Cras mattis consectetur purus sit amet fermentum. Nullam quis risus eget urna mollis ornare vel eu leo. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam.</p>
+                    </article>
+                </div>
+                <a href="#" class="btn btn-default"><i class="icon-mail-alt"></i>hire me</a>
+            </div>
         </section>
+        <section>
+            <div class='hdr' data-flex='row-center'>
+                <h2>my portfolio</h2>
+            </div>
+            <div class="portfolio">
+                <div class="portfolio-hdr">
+                    <h3>conceptual</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+                </div>
+                <div class="img-wrap" data-flex="row--between">
+                    <figure data-flex>
+                       <img src="/images/img1.png"> 
+                    </figure>
+                    <figure data-flex>
+                       <img src="/images/img2.png"> 
+                    </figure>
+                    <figure data-flex>
+                       <img src="/images/img3.png"> 
+                    </figure>
+                </div>
+                <div class="portfolio-hdr">
+                    <h3>black & white</h3>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. </p>
+                </div>
+                <div class="img-wrap" data-flex="row--between">
+                    <figure data-flex>
+                       <img src="/images/img4.png"> 
+                    </figure>
+                    <figure data-flex>
+                       <img src="/images/img5.png"> 
+                    </figure>
+                   <figure data-flex>
+                       <img src="/images/img3.png"> 
+                    </figure>
+                </div>
+            </div>
+        </section>
+        <section class="blog-wrap">
+            <div class='hdr' data-flex='row-center'>
+                <h2>latest blog posts</h2>
+            </div>
+            <div class="blog" data-flex="row--between">
+                <div class="post" data-flex="col">
+                    <figure data-flex="col">
+                        <img src="/images/img6.png">
+                        <figcaption data-flex="row-center-between">
+                            <span><time>21 may 2013</time>&ensp;|&ensp;macro,photography</span>
+                            <a href="#">27<i class="icon-heart"></i></a>
+                        </figcaption>
+                    </figure>
+                    <article>
+                        <h5><a href="#">Tortor Ullamcorper Fringilla Cras Euismod</a></h5>
+                        <p>Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus  augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+                    </article>
+                </div>
+                <div class="post" data-flex="col">
+                    <figure data-flex="col">
+                        <img src="/images/img6.png">
+                        <figcaption data-flex="row-center-between">
+                            <span><time>15 may 2013</time>&ensp;|&ensp;macro,photography</span>
+                            <a href="#">17<i class="icon-heart"></i></a>
+                        </figcaption>
+                    </figure>
+                    <article>
+                        <h5><a href="#">Maecenas sed diam eget risus varius blandit</a></h5>
+                        <p>Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus  augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+                    </article>
+                </div>
+                <div class="post" data-flex="col">
+                    <figure data-flex="col">
+                        <img src="/images/img6.png">
+                        <figcaption data-flex="row-center-between">
+                            <span><time>11 may 2013</time>&ensp;|&ensp;macro,photography</span>
+                            <a href="#">53<i class="icon-heart"></i></a>
+                        </figcaption>
+                    </figure>
+                    <article>
+                        <h5><a href="#">Risus Fermentum Malesuada Justo Tellus</a></h5>
+                        <p>Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus  augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+                    </article>
+                </div>
+                <div class="post" data-flex="col">
+                    <figure data-flex="col">
+                        <img src="/images/img6.png">
+                        <figcaption data-flex="row-center-between">
+                            <span><time>09 may 2013</time>&ensp;|&ensp;macro,photography</span>
+                            <a href="#">35<i class="icon-heart"></i></a>
+                        </figcaption>
+                    </figure>
+                    <article>
+                        <h5><a href="#">Fringilla Parturient Consectetur Egestas Inceptos</a></h5>
+                        <p>Donec id elit non mi porta gravida at eget metus. Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus  augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+                    </article>
+                </div>
+            </div>
+        </section>
+        <section class="contacts-wrap">
+            <div class='hdr' data-flex='row-center'>
+                <h2>get in touch</h2>
+            </div>
+            <div class="contacts" data-flex>
+                <form action="#" method="post" data-flex="col">
+                    <input type="text" placeholder="your name">
+                    <input type="text" placeholder="e-mail">
+                    <input type="text" placeholder="the subject">
+                    <textarea placeholder="your message"></textarea>
+                    <div>
+                        <button><span class="btn btn-default"><i class="icon-mail-alt"></i><em>submit</em></span></button>
+                    </div>                    
+                </form>
+                <div>
+                    <h3>Contact details</h3>
+                    <p>Fusce dapibus, tellus commodo, tortor mauris condimentum utellus fermentum, porta sem malesuada magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia.</p>
+                    <ul>
+                        <li data-flex="row-center"><i class="icon-location"></i>Moonshine Street No: 14/05</li>
+                        <li data-flex="row-center"><i class="icon-phone"></i>Light City, Jupiter</li>
+                        <li data-flex="row-center"><i class="icon-letter"></i>0247 541 65 87</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <footer data-flex="col-center">
+            <span>&copy; 2013 Kyte. All rights reserved. Theme by elemis.</span>
+            <div class="social" data-flex="row-center-center">
+                    <a href="#" class="btn btn-social"><i class="icon-twitter-footer"></i></a>
+                    <a href="#" class="btn btn-social"><i class="icon-instagram-footer"></i></a>
+                    <a href="#" class="btn btn-social"><i class="icon-dribble-footer"></i></a>
+                    <a href="#" class="btn btn-social"><i class="icon-mail-footer"></i></a>
+                </div>
+        </footer>
     </body>
 </html>
---
+++ zeences.iv/tpls/home.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Zeences</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link href='https://fonts.googleapis.com/css?family=Arvo:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <div class="wrap">
+            <header>
+                <section class="header" data-flex="row-center-between">
+                    <a href="#" class="logo" data-flex="row-center"><i class="icon-logo"></i>zeences design</a>
+                    <div class="social" data-flex="row-center">
+                        <a href="#"><i class="icon-rss"></i></a>
+                        <a href="#"><i class="icon-fl"></i></a>
+                        <a href="#"><i class="icon-fb"></i></a>
+                        <a href="#"><i class="icon-db"></i></a>
+                        <a href="#"><i class="icon-tw"></i></a>
+                        <a href="#"><i class="icon-in"></i></a>
+                        <a href="#"><i class="icon-gp"></i></a>
+                    </div>
+                </section>
+                <nav class="nav" data-flex>
+                    <a href="#" class="btn btn-nav active">Home</a>
+                    <a href="#" class="btn btn-nav">Portfolio</a>
+                    <a href="#" class="btn btn-nav">Gallery</a>
+                    <a href="#" class="btn btn-nav">Service</a>
+                    <a href="#" class="btn btn-nav">Blog</a>                    
+                    <a href="#" class="btn btn-nav">Contact</a>
+                </nav>
+            </header>
+            <div class="content-wrap">
+                <section class="slider-wrap" data-flex="col-center">
+                    <figure data-flex>
+                        <img src="/images/img1.png">
+                    </figure>
+                    <div data-flex="row-center">
+                        <a class="btn btn-thumb"><i class="icon-prev-lg"></i></a>
+                        <a class="btn btn-control"></a>
+                        <a class="btn btn-control"></a>
+                        <a class="btn btn-control"></a>
+                        <a class="btn btn-control"></a>
+                        <a class="btn btn-thumb"><i class="icon-next-lg"></i></a>
+                    </div>
+                    <p>Aenean fringilla aliquam est, a porttitor arcu euismod et. Maecenas rutrum erat at arcu porta interdum. Cras convallis ligula ac risus imperdiet placerat.</p>
+                </section>
+                <section class="services-wrap">
+                    <h3>what we do</h3>
+                    <div class="services" data-flex="row--between">                           
+                            <p>Vivamus sodales pharetra libero, a tincidunt nisi condimentum a. Quisque non sodales nislero. Integer a tellus eget arcu fringilla molestie quis vel eros. Praesent fermentum vulputate purusa ornare. Vivamus imperdiet, quam et adipiscing placerat, nibh odio scelerisque risus, vitaere accumsan mi justo et quam. Pellentesque quis felis quis tortor porttitor lobortis nec ac risus.</p>
+                        <ul class="list list-default">
+                            <li>Vivamus at magna non ipsum.</li>
+                            <li>Etiam pharetra pretium eros.</li>
+                            <li>Curabitur porttitor ipsum nec magna.</li>
+                            <li>Maecenas cursus arcu a nulla tristique.</li>
+                        </ul>
+                    </div>
+                    <div class="grid" data-flex="row--between">
+                        <article data-flex="col-center">
+                            <i class="icon-monitor"></i>
+                            <h4>web design</h4>
+                            <p>Vivamus soder pharetra libero atincidunt, nisi condimentum Quisque non sodales nislero.</p>
+                        </article>
+                        <article data-flex="col-center">
+                            <i class="icon-photo"></i>
+                            <h4>photography</h4>
+                            <p>Vivamus soder pharetra libero atincidunt, nisi condimentum Quisque non sodales nislero.</p>
+                        </article> 
+                        <article data-flex="col-center">
+                            <i class="icon-movie"></i>
+                            <h4>motion graphic</h4>
+                            <p>Vivamus soder pharetra libero atincidunt, nisi condimentum Quisque non sodales nislero.</p>
+                        </article> 
+                        <article data-flex="col-center">
+                            <i class="icon-graph"></i>
+                            <h4>optimization</h4>
+                            <p>Vivamus soder pharetra libero atincidunt, nisi condimentum Quisque non sodales nislero.</p>
+                        </article> 
+                    </div>
+                </section>
+                <section class="works-slider">
+                    <div data-flex="row--between">
+                        <h3>latest works</h3>
+                        <div data-flex>
+                            <a href="#" class="btn btn-thumb"><i class="icon-prev-sm"></i></a>
+                            <a href="#" class="btn btn-thumb"><i class="icon-next-sm"></i></a>
+                        </div>
+                    </div>
+                    <div class="grid" data-flex="row--between">
+                        <figure data-flex>
+                            <img src="/images/img2.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="/images/img2.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="/images/img2.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="/images/img2.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="/images/img2.png">
+                        </figure>
+                    </div>
+                </section>
+                <footer class="footer" data-flex="row--between">
+                    <div>
+                        <h4>latest posts</h4>
+                        <div>
+                            <div class="post" data-flex="row-start">
+                                <time>
+                                    <span>22</span>
+                                    <small>jun</small>
+                                </time>
+                                <p><a href="#">Vivamus soder pharetra libero atincidunt</a></p>
+                            </div>
+                            <div class="post" data-flex="row-start">
+                                <time>
+                                    <span>14</span>
+                                    <small>may</small>
+                                </time>
+                                <p><a href="#">Curabitur at ante sed nunc euismod sodales</a></p>
+                            </div>
+                            <div class="post" data-flex="row-start">
+                                <time>
+                                    <span>11</span>
+                                    <small>may</small>
+                                </time>
+                                <p><a href="#">Nulla euismod augue sed ante vestibulum</a></p>
+                            </div>
+                            <div class="post" data-flex="row-start">
+                                <time>
+                                    <span>08</span>
+                                    <small>apr</small>
+                                </time>
+                                <p><a href="#">Sed purus neque, consequat eget mattis </a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h4>flickr</h4>
+                        <div class="grid" data-flex="row--between">
+                            <a href="#" data-flex>
+                                <img src="/images/img3.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/img3.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/img3.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/img3.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/img3.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/img3.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/img3.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/img3.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/img3.png">
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <h4>twitter</h4>
+                        <ul class="list list-default">
+                            <li>
+                                <a href="#">Vivamus soder pharetra libero atincidunt Curabitur at ante sed nunc euismod sodales.</a>
+                            </li>
+                            <li>
+                                <a href="#">Vivamus soder pharetra libero atincidunt Curabitur at ante sed nunc euismod sodales.</a>
+                            </li>
+                            <li>
+                                <a href="#">Vivamus soder pharetra libero atincidunt Curabitur at ante sed nunc euismod sodales.</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h4>contact form</h4>
+                        <form action="#" method="post" data-flex="col">
+                            <input type="text">
+                            <input type="text">
+                            <textarea></textarea>
+                            <div>
+                            <button><span class="btn btn-default">Submit</span></button>
+                            </div>
+                        </form>
+                    </div>
+                </footer>
+            </div>
+            <section class="copyright" data-flex="row-center-center">
+                <span>&copy; 2011 Zeences Design. All Right Reserved.</span>
+            </section>
+        </div>
+    </body>
+</html>
