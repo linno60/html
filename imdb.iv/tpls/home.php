21e52467e6399e053cb624cdda6738ceead12cba
Ivan <auzp@ukr.net>
2015-09-08 20:56:10 +0300

    commit

---
+++ imdb.iv/tpls/home.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>IMDB</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <div class="lwrap">
+            <header>
+
+            </header>
+            <section class="complicated-grid">
+                <div data-flex="2cols">
+                    <div>
+                        <div data-flex="row-start">
+                            <figure data-flex>
+                                <img src="/images/1.png">
+                            </figure>
+                            <figure data-flex>
+                                <img src="/images/2.png">
+                            </figure>
+                            <figure data-flex>
+                                <img src="/images/3.png">
+                            </figure>
+                            <figure data-flex>
+                                <img src="/images/4.png">
+                            </figure>
+                        </div>
+                        <div data-flex="row-start">
+                            <div data-flex="col">
+                                <figure data-flex>
+                                    <img src="/images/5.png">
+                                </figure>
+                                <figure data-flex>
+                                    <img src="/images/6.png">
+                                </figure>
+                            </div>
+                            <figure data-flex="row-start">
+                                <img src="/images/7.png">
+                            </figure>
+                            <div data-flex="col-start-end">
+                                <figure data-flex>
+                                    <img src="/images/8.png">
+                                </figure>
+                                <figure data-flex>
+                                    <img src="/images/9.png">
+                                </figure>
+                            </div>
+                        </div>
+                        <div data-flex="row-start">
+                            <figure data-flex>
+                                <img src="/images/26.png">
+                            </figure>
+                            <figure data-flex>
+                                <img src="/images/27.png">
+                            </figure>
+                            <figure data-flex>
+                                <img src="/images/28.png">
+                            </figure>
+                            <figure data-flex>
+                                <img src="/images/29.png">
+                            </figure>
+                        </div>
+                    </div>
+                    <div data-flex="row-start">
+                        <div data-flex="col">                            
+                            <figure data-flex>
+                                <img src="/images/10.png">
+                            </figure>
+                            <div data-flex>
+                                <figure data-flex>
+                                    <img src="/images/11.png">
+                                </figure>
+                                <figure data-flex>
+                                    <img src="/images/12.png">
+                                </figure>
+                            </div>
+                        </div>
+                        <div data-flex="col">
+                            <div data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/13.png">
+                                </figure>
+                                <figure data-flex>
+                                    <img src="/images/14.png">
+                                </figure>
+                            </div>
+                            <figure data-flex>
+                                <img src="/images/15.png">
+                            </figure>
+                            <div data-flex="row-start">
+                                <div>
+                                    <figure data-flex>
+                                        <img src="/images/16.png">
+                                    </figure> 
+                                </div>
+                                <div data-flex="col">
+                                    <figure data-flex>
+                                        <img src="/images/17.png">
+                                    </figure>
+                                    <figure data-flex>
+                                        <img src="/images/18.png">
+                                    </figure>
+                                </div>                                
+                            </div>
+                        </div>
+                        <div data-flex="col">
+                            <div data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/19.png">
+                                </figure>
+                                <figure data-flex>
+                                    <img src="/images/20.png">
+                                </figure>
+                            </div>                            
+                            <figure data-flex>
+                                <img src="/images/21.png">
+                            </figure>
+                            <div data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/22.png">
+                                </figure>
+                                <figure data-flex>
+                                    <img src="/images/23.png">
+                                </figure>
+                            </div>
+                            <div data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/24.png">
+                                </figure>
+                                <figure data-flex>
+                                    <img src="/images/25.png">
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div data-flex="2cols">
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
---
+++ theyalow.iv/tpls/home.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Theyalow</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <header>
+            <section class="header" data-flex="row-end-between">
+                <a href="#" class="logo" data-flex="row-center"><img src="/images/logo.png">theyalow<div><span>craft by css author.com</span></div></a>
+                <div class="nav-wrap" data-flex>
+                    <div class="nav-wraper" data-flex>
+                        <nav class="nav" data-flex>
+                            <a href="#" class="btn" data-flex="row-center-center"><i class="icon-user"></i>about us</a>
+                            <a href="#" class="btn" data-flex="row-center"><i class="icon-service"></i>services</a>
+                            <a href="#" class="btn" data-flex="row-center"><i class="icon-letter"></i>contact</a>
+                        </nav>
+                    </div>
+                </div>                
+            </section>
+            <section class="slider-wrap" data-flex="row-center-between">                
+                <a href="#" class="btn btn-slide"><i class="icon-prev"></i></a>
+                <div class="slider" data-flex="row-center-between">
+                    <iframe src="https://www.youtube.com/embed/hO4UaLG0zY8" frameborder="0" allowfullscreen></iframe>
+                    <h1>the cool<br><span>the minimal</span></h1>
+                </div>
+                <a href="#" class="btn btn-slide"><i class="icon-next"></i></a>
+            </section>
+        </header>
+        <section class="advantages-wrap">
+            <div class="advantages" data-flex="row--between">
+                <div data-flex="col-center-center">
+                    <i class="icon-cloud"></i>
+                    <span>better design</span>
+                </div>
+                <div data-flex="col-center-center">
+                    <i class="icon-leave"></i>
+                    <span>customise</span>
+                </div>
+                <div data-flex="col-center-center">
+                    <i class="icon-free"></i>
+                    <span>its free</span>
+                </div>
+            </div>
+        </section>
+        <section class="banners-wrap">
+            <div class="banners">
+                <h2>lorem ipsum dolor sit amet</h2>
+                <div data-flex="row--between">
+                    <div data-flex>
+                        <div class="banners-item">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor inci&shy;didunt ut</p>
+                            <a href="#"><i class="icon-plus"></i></a>
+                        </div>
+                        <iframe src="https://www.youtube.com/embed/b0NHrFNZWh0" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                    <div class="banners-item social" data-flex="row-center-center">
+                        <span>22</span>
+                        <i class="icon-twitter"></i>
+                        <a href="#"><i class="icon-plus"></i></a>
+                    </div>
+                </div>
+                <div class="lbl lbl-date" data-flex="col">
+                    <time>25</time>
+                    april
+                </div>
+                <div class="scroll-wrap">
+                    <div class="scroll"></div>
+                </div>
+            </div>
+        </section>
+        <section class="conditions-wrap">
+            <div class="conditions">
+                <h2>lorem ipsum dolor sit amet</h2>
+                <div data-flex="row--between">
+                    <div class="img-wrap" data-flex="row-end">
+                        <div class="lbl lbl-default">its 100% free</div>
+                        <figure data-flex>
+                            <img src="images/desktop.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="images/table.png">
+                        </figure>
+                        <figure data-flex>
+                            <img src="images/smartphone.png">
+                        </figure>
+                    </div>
+                    <ul class="list list-default">
+                        <li>Sed ut perspiciatis unde omnis</li>
+                        <li>Lorem upsum dolor sit amet, con&shy;sectetur adipisicing</li>
+                        <li>Sed ut perspiciatis unde omnis</li>
+                        <li>Lorem ipsum dolor sit amet, con&shy;sectetur adipisicing</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <section class="footer" data-flex="row-center-between">
+                <a href="#" class="logo" data-flex="row-center"><img src="/images/logo-footer.png">theyalow</a>
+                <span>Designed by <a href="#">cssauthor.com</a></span>
+            </section>
+        </footer>
+    </body>
+</html>
