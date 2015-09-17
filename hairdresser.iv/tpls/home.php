4d5154b98949b94407427358376eb6342d2fcfda
Ivan <auzp@ukr.net>
2015-09-17 09:36:34 +0300

    commit

---
+++ hairdresser.iv/tpls/home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Hairdresser</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <header>
+            <div class="lwrap">
+                <div class="nav-wrap" data-flex="row-center-between">
+                <a href="#" class="logo">hairdresser</a>
+                <nav class="nav" data-flex="row-center">
+                    <a href="#" class="btn btn-nav active">home</a>
+                    <a href="#" class="btn btn-nav">tour</a>
+                    <a href="#" class="btn btn-nav">buzz</a>
+                    <a href="#" class="btn btn-nav">pricing</a>
+                    <a href="#" class="btn btn-nav">sign up</a>
+                    <a href="#" class="btn btn-nav">legal</a>
+                    <a href="#" class="btn btn-nav">contact</a>
+                </nav>
+            </div>
+            <div class="header" data-flex="row--between">
+                <div data-flex="col-start">
+                    <h1>try out the free trial!</h1>
+                    <h3>by simply filling out the form below</h3>
+                    <form action="#" method="post" data-flex="col-start">
+                        <input type="text" placeholder="your name">
+                        <input type="text" placeholder="your email address">
+                        <input type="text" placeholder="password">
+                    </form>
+                    <a href="#" dataf-flex class="btn btn-start">
+                        <em data-flex="col-start">
+                            <span>start the free trial</span>
+                            <small>and enjoy it for 2 months!</small>
+                        </em>
+                        <div class="btn"><i class="icon-check"></i></div>
+                    </a>
+                    <div data-flex>
+                        <a href="#"><img src="/images/appstore.png"></a>
+                        <a href="#"><img src="/images/gp.png"></a>
+                    </div>
+                </div>
+                <div>
+                    <figure>
+                        <img src="/images/phone1.png">
+                    </figure>
+                </div>
+            </div>
+            </div>            
+        </header>
+    </body>
+</html>
--- mihstore.iv/tpls/home.php
+++ mihstore.iv/tpls/home.php
@@ -15,7 +15,7 @@
                     <span>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog.</span>
                     <nav class="nav-top" data-flex="row-center">
                         <a href="#">my account</a>
-                        <a href="#">whishlist</a>
+                        <a href="#">wishlist</a>
                         <a href="#">contact</a>
                     </nav>
                 </section>
@@ -33,6 +33,13 @@
                     </div>
                 </section>
                 <section class="nav-wrap" data-flex="row-center-between">
+                    
+                    <div class="cart-wrap" data-flex="row-center">
+                        <span class="cart">shopping cart:</span>
+                        <span class="items">3 items</span>
+                        <span class="price">70$</span>
+                        <a href="#" data-flex="row-center-center"></a>
+                    </div>
                     <nav class="nav" data-flex="row-center">
                         <a href="#" class="active">products</a>
                         <a href="#">ideas</a>
@@ -40,25 +47,35 @@
                         <a href="#">gifts</a>
                         <a href="#">stores</a>
                     </nav>
-                    <div class="cart-wrap">
-                        <span>shopping cart</span>
-                    </div>
                 </section>
                 <section class="page-wrap" data-flex="row-center">
-                    <span>page title</span>
+                    <span>page title&emsp;&emsp; |</span>
                     <p>Page description The quick, brown fox jumps over a lazy dog. DJs flock by when TV ax quiz prog.</p>
                 </section>
             </header>
             <section class="content-wrap" data-flex>
                 <div class="content">
-                    <div class="slider">
-                        <img src="/images/img.png">
+                    <div class="slider" data-flex="row-end">
+                        <div class="lbl lbl-shop" data-flex="col">
+                            <span>Shop <b>women</b></span>
+                            <small>Shoes fall 2011</small>
+                        </div>
+                        <figure data-flex>
+                            <img src="/images/1.jpg">
+                        </figure>
+                        <div data-flex="col-end-end">
+                            <a href="#">view more</a>
+                            <figure data-flex>
+                                <img src="/images/2.jpg">
+                            </figure>
+                        </div>
                     </div>
                     <div class="products">
-                        <h2 class="hdr">reatured products</h2> 
+                        <h2 class="hdr">featured products</h2> 
                         <div class="grid" data-flex="row--between">                            
                             <figure data-flex="col">
                                 <a href="#" data-flex="row--center">
+                                    <div class="lbl lbl-green">-50%</div>
                                     <img src="/images/img3.png">
                                 </a>
                                 <figcaption data-flex="row-center-between">
@@ -68,6 +85,7 @@
                             </figure>
                             <figure data-flex="col">
                                 <a href="#" data-flex="row--center">
+                                    <div class="lbl lbl-red">-35%</div>
                                     <img src="/images/img4.png">
                                 </a>
                                 <figcaption data-flex="row-center-between">
@@ -77,6 +95,7 @@
                             </figure>
                             <figure data-flex="col">
                                 <a href="#" data-flex="row--center">
+                                    <div class="lbl lbl-blue">-20%</div>
                                     <img src="/images/img5.png">
                                 </a>
                                 <figcaption data-flex="row-center-between">
@@ -139,14 +158,145 @@
                                 </figcaption>
                             </figure>
                         </div>
+                        <div class="pagination" data-flex="row-center-center">
+                            <a href="#" class="btn btn-pager active">1</a>
+                            <a href="#" class="btn btn-pager">2</a>
+                            <a href="#" class="btn btn-pager">3</a>
+                            <a href="#" class="btn btn-pager">4</a>
+                            <a href="#" class="btn btn-pager">5</a>
+                            <a href="#" class="btn btn-pager">6</a>
+                        </div>
                     </div>
                 </div>
-                <aside class="aside">
-                    <h2 class="hdr">sort products</h2>
+                <aside class="aside" data-flex="col--between">
+                    <div class="aside-wrap">
+                        <h2 class="hdr">sort products</h2>
+                        <form action="#" method="post" data-flex="col">
+                           <span class="select">
+                                <select>
+                                    <option selected>price</option>
+                                    <option>to cheap</option>
+                                    <option>to expensive</option>          
+                                </select>
+                            </span>
+                            <span class="select">
+                                <select>
+                                    <option selected>style</option>
+                                    <option>sport</option>
+                                    <option>classic</option>          
+                                </select>
+                            </span>
+                            <span class="select">
+                                <select>
+                                    <option selected>colour</option>
+                                    <option>black</option>
+                                    <option>white</option>          
+                                </select>
+                            </span>
+                            <span class="select">
+                                <select>
+                                    <option selected>season</option>
+                                    <option>winter</option>
+                                    <option>summer</option>          
+                                </select>
+                            </span>
+                            <span class="select">
+                                <select>
+                                    <option selected>rating</option>
+                                    <option>to top</option>
+                                    <option>to bottom</option>          
+                                </select>
+                            </span>
+                        </form>
+                        <h2 class="hdr">best sellers</h2>
+                        <div>
+                            <article data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/img9.png">
+                                </figure>
+                                <div data-flex="col">
+                                    <a href="#">Product name here</a>
+                                    <span>Description Lorem ipsum sit am</span>
+                                    <div data-flex>
+                                        <span>Price:<em>$110</em>|</span><span>Country:<em>US</em></span>
+                                    </div>
+                                </div>
+                            </article>
+                            <article data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/img10.png">
+                                </figure>
+                                <div data-flex="col">
+                                    <a href="#">Product name here</a>
+                                    <span>Description Lorem ipsum sit am</span>
+                                    <div data-flex>
+                                        <span>Price:<em>$110</em>|</span><span>Country:<em>US</em></span>
+                                    </div>
+                                </div>
+                            </article>
+                            <article data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/img9.png">
+                                </figure>
+                                <div data-flex="col">
+                                    <a href="#">Product name here</a>
+                                    <span>Description Lorem ipsum sit am</span>
+                                    <div data-flex>
+                                        <span>Price:<em>$110</em>|</span><span>Country:<em>US</em></span>
+                                    </div>
+                                </div>
+                            </article>
+                            <article data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/img10.png">
+                                </figure>
+                                <div data-flex="col">
+                                    <a href="#">Product name here</a>
+                                    <span>Description Lorem ipsum sit am</span>
+                                    <div data-flex>
+                                        <span>Price:<em>$110</em>|</span><span>Country:<em>US</em></span>
+                                    </div>
+                                </div>
+                            </article>
+                        </div>
+                        <h2 class="hdr">payment options</h2>
+                        <div class="grid" data-flex="row--between">
+                            <a href="#" data-flex>
+                                <img src="/images/paypal.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/visa.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/wirecard.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/mastercard.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/maestro.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/cash.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/paypal.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/visa.png">
+                            </a>
+                            <a href="#" data-flex>
+                                <img src="/images/wirecard.png">
+                            </a>
+                        </div>
+                    </div>
+                    <div data-flex="row--end">
+                        <div class="aside-bottom"></div>
+                    </div>
                 </aside>
             </section>
             <footer data-flex="row-center-between">
-                <span>&copy All rights reserved - ICEWave Design</span>
+                <span>&copy All Rights Reserved - ICEWave Design</span>
                 <a href="#">go to top!</a>
             </footer>
         </div>
