6753bf4c6d1615ccbee2c110f9bcc71172572a56
Ivan <auzp@ukr.net>
2015-09-19 10:29:23 +0300

    commit

---
+++ seabird.iv/tpls/home.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Seabird</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <div class="lwrap">
+            <div class="wrap">
+                <div>
+                    <header>
+                        <a href="#" class="logo">Seabird.</a>
+                        <div class="nav-wrap" data-flex="row-center-between">
+                            <nav class="nav" data-flex>
+                                <a href="#" class="btn">home</a>
+                                <a href="#" class="btn">portfolio</a>
+                                <a href="#" class="btn">blog</a>
+                                <a href="#" class="btn">pages</a>
+                                <a href="#" class="btn">features</a>
+                                <a href="#" class="btn">contact</a>
+                            </nav>
+                            <div class="social" data-flex="row-center">
+                                <a href="#"><i class="icon-rss"></i></a>
+                                <a href="#"><i class="icon-fb"></i></a>
+                                <a href="#"><i class="icon-tw"></i></a>
+                            </div>
+                        </div>
+                    </header>
+                    <section class="slider-wrap" data-flex="row-center-between">
+                        <div class="slider-side"><a href="#" class="btn btn-slide"><i class="icon-prev"></i></a></div>
+                        <div class="slider" data-flex="col-center">
+                            <h1>We design & build responsive themes</h1>
+                            <h2>Maecenas sed diam varius blandit magna. Duis mollis commodo luctus.</h2>
+                            <figure>
+                                <img src="/images/slider.png">
+                            </figure>
+                            <div class="controls">
+                                <a href="#" class="btn"></a>
+                                <a href="#" class="btn active"></a>
+                                <a href="#" class="btn"></a>
+                                <a href="#" class="btn"></a>
+                            </div>
+                        </div>
+                        <div class="slider-side"><a href="#" class="btn btn-slide"><i class="icon-next"></i></a></div>
+                    </section>
+                    <section class="services">
+                        <h2>Interface Design is Important for Us</h2>
+                        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec sed odio dui.</p>
+                        <div class="grid" data-flex="row--between">
+                            <a href="#" data-flex="col-center">
+                                <div class="lbl lbl-service"><i class="icon-laptop"></i></div>
+                                <span>Interface Design</span>
+                            </a>
+                            <a href="#" data-flex="col-center">
+                                <div class="lbl lbl-service"><i class="icon-film"></i></div>
+                                <span>Motion Graphic</span>
+                            </a>
+                            <a href="#" data-flex="col-center">
+                                <div class="lbl lbl-service"><i class="icon-camera"></i></div>
+                                <span>Photography</span>
+                            </a>
+                            <a href="#" data-flex="col-center">
+                                <div class="lbl lbl-service"><i class="icon-brush"></i></div>
+                                <span>Illustration</span>
+                            </a>
+                        </div>
+                    </section>
+                    <section class="works">
+                        <h3>our awesome works</h3>
+                        <div class="grid" data-flex="row--between">
+                            <article>
+                                <a href="#" data-flex>
+                                    <img src="/images/img.png">
+                                </a>
+                                <h4><a href="#">Nobis Business Card</a></h4>
+                                <span>Business Card, Graphic</span>
+                                <p>Sed posuere consectetur est at lobortis. Porta sagittis lacus vel augue laoreet rutrum.</p>
+                            </article>
+                            <article>
+                                <a href="#" data-flex>
+                                    <img src="/images/img.png">
+                                </a>
+                                <h4><a href="#">New Fun Project</a></h4>
+                                <span>Web Design, Application</span>
+                                <p>Nulla vitae elit libero, a pharetra augue. Donec sed odio dui. Vestibulum id ligula porta felis.</p>
+                            </article>
+                            <article>
+                                <a href="#" data-flex>
+                                    <img src="/images/img.png">
+                                </a>
+                                <h4><a href="#">Passionaries Branding Cover</a></h4>
+                                <span>Branding, Graphic Design</span>
+                                <p>Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus.</p>
+                            </article>
+                        </div>
+                    </section>
+                </div>
+                <footer>
+                    <div class="grid" data-flex="row--between">
+                        <div>
+                            <h4>get in touch</h4>
+                            <p>Fusce dapibus, tellus commodo, tortor mauris condimentum utellus fermentum, porta sem malesuada magna. </p>
+                            <ul>
+                                <li data-flex><i class="icon-location"></i>Moonshine Street No: 14/05<br>Light City, Jupiter</li>
+                                <li data-flex><i class="icon-phone"></i>0247 541 65 87</li>
+                                <li data-flex><i class="icon-mail"></i><a href="mailto:support@longwave.com">support@longwave.com</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h4>about us</h4>
+                            <p>Aenean lacinia bibendum nulla sed leo posuere erat a ante venenatis dapibus posuere velit aliquet. Donec ullamcorper metus auctor fringilla. Nullam quis risus.</p>
+                            <p>Vestibulum id ligula porta  euismod semper. Maecenas faucibus mollis.Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+                        </div>
+                        <div class="posts">
+                            <h4>popular posts</h4>
+                            <p>Vivamus sagittis lacus vel augue laoreet rutrum dolor auctor tellus commodo<br><time>14 Nov, 2012</time></p>
+                            <p>Scelerisque nisl consectetur +et.<br><time>22 May, 2012</time></p>
+                            <p>Pellentesque ornare sem lacinia quam venenatis vestibulum, fusce dapibus.<br><time>15 July, 2012</time></p>
+                        </div>
+                    </div>
+                    <div class="copyright" data-flex="row-center">
+                        <span>&copy; 2013 Seabird. All Rights Reserved.</span>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ tmps.iv/tpls/home.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>TMPS</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link type="text/css" rel="stylesheet" href="/css/reset.css">
+        <link type="text/css" rel="stylesheet" href="/css/reset-elements.css">
+        <link type="text/css" rel="stylesheet" href="/css/styles.css">    
+    </head>
+    <body>
+        <header>
+            <div class="lwrap" data-flex="row-center-between">
+                <a href="#" class="logo">site name</a>
+                <nav class="nav" data-flex>
+                    <a href="#" class="btn btn-nav active">home</a>
+                    <a href="#" class="btn btn-nav">about</a>
+                    <a href="#" class="btn btn-nav">services</a>
+                    <a href="#" class="btn btn-nav">portfolio</a>
+                    <a href="#" class="btn btn-nav">contact</a>
+                </nav>
+            </div>
+        </header>
+        <div class="slider-wrap">
+            <div class="slider" data-flex="row-end">
+                <div data-flex="col">
+                    <div class="btns">
+                        <a href="#"><i class="icon-prev"></i></a>
+                        <a href="#"><i class="icon-next"></i></a>                    
+                    </div>
+                    <h1 data-flex="row-center-between"><span>clivuam tinc&ensp;</span><div></div></h1>
+                    <h1 data-flex="row-center-between"><span>nunc dignissim risus id metus</span><div></div></h1> 
+                    <a href="#">http://wallpaperswide.com/</a>
+                </div>  
+            </div>
+        </div>
+        <div class="services">
+            <div class="lwrap grid" data-flex="row--between">
+                <article data-flex="row-start">
+                    <div class="lbl lbl-service">
+                        <i class="icon-rocket"></i> 
+                    </div>
+                    <div>
+                        <h3>Aliquam tincidunt</h3>
+                        <p>Donec nec justo eget felis facilisis fermen&shy;tum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesq <a href="#">=> more</a></p>
+                    </div>
+                </article>
+                <article data-flex="row-start">
+                    <div class="lbl lbl-service">
+                        <i class="icon-shield"></i> 
+                    </div>
+                    <div>
+                        <h3>cras ornare tristiqu</h3>
+                        <p>Jonec nec justo eget felis facilisis fermen&shy;tum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesq <a href="#">=> more</a></p>
+                    </div>
+                </article>
+                <article data-flex="row-start">
+                    <div class="lbl lbl-service">
+                        <i class="icon-grenade"></i> 
+                    </div>
+                    <div>
+                        <h3>vivamus vestibulum</h3>
+                        <p>Conec nec justo eget felis facilisis fermen&shy;tum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesq <a href="#">=> more</a></p>
+                    </div>
+                </article>
+            </div>
+        </div>
+        <div class="content-wrap">
+            <div class="lwrap">
+                <div class="about" data-flex="row-start-between">
+                    <div>
+                        <h2>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor sit amet orci. Aenean dignissim pellentesque felis</h2>
+                        <h4>Civamus vestibulum nulla nec ante</h4>
+                        <div class="grid" data-flex="row--between">
+                            <article>
+                                <h6>Sed egestas anteet </h6>
+                                <p>Eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. </p>
+                                <a href="#" class="btn btn-default">more</a>
+                            </article>
+                            <article>
+                                <h6>Aenean egestas anteet </h6>
+                                <p>Lorem pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. Ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. </p>
+                                <a href="#" class="btn btn-default">more</a>
+                            </article>
+                        </div>
+                    </div>
+                    <figure data-flex>
+                        <img src="/images/circle.png">
+                    </figure>
+                </div>
+                <div class="subscribe" data-flex="2cols">
+                    <div data-flex="row-start">
+                        <div class="lbl lbl-subscribe">
+                            <i class="icon-user"></i>
+                        </div>
+                        <div>
+                            <h3>Donec nec justo eget</h3>
+                            <blockquote>
+                                “ Ede semper est, vitae luctus metus libero eu augue. Morb sipo ipiscing, commodo quis, gravida  ...”
+                                <cite>Kohn Doe / company name</cite>
+                            </blockquote>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>Morbi vitae luctus</h3>
+                        <form action="#" method="post" data-flex="row--between">
+                            <input type="text" placeholder="enter your email id here...">
+                            <button><span class="btn btn-default"><em>subscribe</em><i class="icon-arrow"></i></span></button>
+                        </form>
+                    </div>
+                </div>
+                <div class="info" data-flex="3cols">
+                    <div>
+                        <h3>aliquam tincidunt</h3>
+                        <div>
+                            <article class="item" data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/img1.png">
+                                </figure>
+                                <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor<a href="#"> more =></a></p>
+                            </article>
+                            <article class="item" data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/img2.png">
+                                </figure>
+                                <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor<a href="#"> more =></a></p>
+                            </article>
+                            <article class="item" data-flex="row-start">
+                                <figure data-flex>
+                                    <img src="/images/img3.png">
+                                </figure>
+                                <p>Donec nec justo eget felis facilisis fermentum. Aliquam porttitor<a href="#"> more =></a></p>
+                            </article>
+                        </div>                        
+                    </div>
+                    <div>
+                        <h3>nunc dignissim risu</h3>
+                        <form action="#" method="post" data-flex="col">
+                            <label>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus liber</label>
+                            <input type="next" placeholder="name ...">
+                            <input type="next" placeholder="email ...">
+                            <textarea placeholder="your query ..."></textarea>
+                            <div><button><span class="btn btn-default"><em>send</em><i class="icon-arrow"></i></span></button></div>
+                        </form>
+                    </div>
+                    <div>
+                        <h3>aliquam tincidunt</h3>
+                        <div class="contacts">
+                            <div class="item" data-flex="row-start">
+                                <i class="icon-address"></i>
+                                <div data-flex="col-start">
+                                    <h5>Address</h5>
+                                    <span>some block, steet number, city name country</span>
+                                </div>
+                            </div>
+                            <div class="item" data-flex="row-start">
+                                <i class="icon-phone"></i>
+                                <div data-flex="col-start">
+                                    <h5>Phone</h5>
+                                    <span>(000) 777 777 7777</span>
+                                    <span>(000) 888 888 8888</span>
+                                </div>
+                            </div>
+                            <div class="item" data-flex="row-start">
+                                <i class="icon-mail"></i>
+                                <div data-flex="col-start">
+                                    <h5>Email us</h5>
+                                    <a href="mailto:info@sitename.com">info@sitename.com</a>
+                                    <a href="mailto:sales@sitename.com">sales@sitename.com</a>
+                                    <a href="mailto:company@sitename.com">company@sitename.com</a>
+                                </div>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <div class="lwrap" data-flex="row-center-between">
+                <div>
+                    <nav data-flex>
+                        <a href="#"></a>
+                        <span>/</span>
+                        <a href="#"></a>
+                        <span>/</span>
+                        <a href="#"></a>
+                        <span>/</span>
+                        <a href="#"></a>
+                        <span>/</span>
+                        <a href="#"></a>
+                        <span>/</span>
+                    </nav>
+                    <p>
+                        <span></span>
+                    </p>
+                </div>
+                <div  class="social" data-flex="row-center">
+                    <a href="#" class="btn btn-social"><i class="icon-fb"></i></a>
+                    <a href="#" class="btn btn-social"><i class="icon-tw"></i></a>
+                    <a href="#" class="btn btn-social"><i class="icon-in"></i></a>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
