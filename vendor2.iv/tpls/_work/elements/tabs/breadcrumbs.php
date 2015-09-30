f259ef7ff9023f115eb5c551461f0fe006c66bed
Ivan <auzp@ukr.net>
2015-09-30 11:13:26 +0300

    commit

--- vendor2.iv/tpls/_work/elements/tabs/breadcrumbs.php
+++
@@ -1,6 +0,0 @@
-<ul class="breadcrumb clearfix">
-    <li><a href="#">Home</a></li>
-    <li><a href="#"><span>></span>Electronics</a></li>
-    <li><a href="#"><span>></span>Mobile</a></li>
-    <li><span>></span>Samsung Galaxy Ace</li>
-</ul>
