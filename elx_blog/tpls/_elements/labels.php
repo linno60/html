00248e085feac6d17463bed504512766d1f75940
Ivan <auzp@ukr.net>
2015-10-10 15:47:16 +0300

    commit

---
+++ elx_blog/tpls/_elements/labels.php
@@ -0,0 +1,4 @@
+<time class="lbl lbl-date">
+    <span>28</span>
+    <small>feb</small>
+</time>
--- elx_blog/tpls/_elements/links.php
+++ elx_blog/tpls/_elements/links.php
@@ -7,3 +7,12 @@
     <a href="#">archives</a>
     <a href="#">contact us</a>
 </nav>
+<ul class="links links-sidebar marker">
+    <a href="#">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </a>
+    <a href="#">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</a></li>
+    <a href="#">When an unknown printer took a galley of type and scrambled it to make a type specimen book. </a>
+    <a href="#">It has survived not only five centuries, but also the leap into electronic typesetting</a>
+    <a href="#">Remaining essentially unchanged. It was popular&shy;ised in the 1960s with the release of</a>
+    <a href="#">Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing </a>
+    <a href="#">Software like Aldus PageMaker including versions of Lorem Ipsum.</a>
+</ul>
