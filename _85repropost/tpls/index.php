27470766a91c767a243fd709184bb5c2fdeb0155
Ivan <auzp@ukr.net>
2015-12-19 17:19:13 +0200

    commit

--- _85repropost/tpls/index.php
+++ _85repropost/tpls/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Index</title>
+        <title>Repropost</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
         <link rel="stylesheet" href="/css/addons/reset.css">
@@ -13,15 +13,56 @@
         <link rel="stylesheet" href="/css/styles.css">  
         <script>
             var LAYOUT_WIDTH = 960;
-            document.documentElement.style.fontSize = window.innerWidth < LAYOUT_WIDTH ? window.innerWidth/LAYOUT_WIDTH+'px' : '1px';
-            window.onresize = function() {
-                document.documentElement.style.fontSize = window.innerWidth < LAYOUT_WIDTH ? window.innerWidth/LAYOUT_WIDTH+'px' : '1px';
+            document.documentElement.style.fontSize = window.innerWidth < LAYOUT_WIDTH ? window.innerWidth / LAYOUT_WIDTH + 'px' : '1px';
+            window.onresize = function () {
+                document.documentElement.style.fontSize = window.innerWidth < LAYOUT_WIDTH ? window.innerWidth / LAYOUT_WIDTH + 'px' : '1px';
             }
         </script>
     </head>
     <body>
-        <h1>This is index page</h1>
-        
+        <header>
+            <div class="topbar">
+                <div class="lwrap" data-flex="axis">
+                    <div data-flex>
+                        <a href="#" data-flex><i class="icon-rss"></i> Subscribe </a>
+                        <span> by </span>
+                        <a href="#"> RSS </a>
+                        <span> or </span>
+                        <a href="#"> Email </a>
+                    </div>
+                    <div class="links-topbar" data-flex='-center end w'>
+                        <a href="#" class="active">Dropdowns</a><div></div>
+                        <a href="#">Elements</a><div></div>
+                        <a href="#">Full Width</a><div></div>
+                        <a href="#">Archives</a><div></div>
+                        <a href="#">Contact</a><div></div>
+                        <a href="#">Buy This Theme</a>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="lwrap">
+                    <div class="header" data-flex="axis">
+                        <a href="/" title="Return to the homepage">
+                            <img src="/images/logo.png" width="234" height="53" alt="Repropost">
+                        </a>
+                        <figure><img src="/images/banner.png" width="468" height="60" alt=""></figure>
+                    </div>
+                    <nav class="links-nav" data-flex data-childs='-center center'>
+                        <a href="#">Home</a>
+                        <a href="#">Culture</a>
+                        <a href="#">Lifestyle</a>
+                        <a href="#" class="active">Worls News</a>
+                        <a href="#">Sport</a>
+                        <a href="#">Travel</a>
+                        <a href="#">Health</a>
+                        <a href="#">Finance</a>
+                        <a href="#">Weird</a>
+                        <a href="#">Celebrity</a>
+                    </nav>
+                </div>
+            </div>
+        </header>
         <script src="/js/jquery/jquery-2.1.4.min.js"></script>
         <script src="/js/tabs-accordions.js"></script>
         <script src="/js/index.js"></script>
