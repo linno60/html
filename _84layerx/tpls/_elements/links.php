9913641545d66c8640a821c247f6d8a0ae4ab5c2
Ivan <auzp@ukr.net>
2015-12-31 19:55:10 +0200

    commit

---
+++ _84layerx/tpls/_elements/links.php
@@ -0,0 +1,46 @@
+<nav class="links-nav" data-flex="vaxis">
+    <a href="#">Archives</a>
+    <a href="#">Talk with us</a>
+    <a href="#">About us</a>
+    <a href="#">Portfolio</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Feedback</a>
+    <a href="#">Latest Posts</a>
+</nav>
+<div class="links-tags" data-flex="w" data-childs="-center center">
+    <a href="#">design</a>
+    <a href="#">blugraphic</a>
+    <a href="#">art</a>
+    <a href="#">buttons</a>
+    <a href="#">website</a>
+    <a href="#">gray design</a>
+    <a href="#">templates</a>
+    <a href="#">graphics</a>
+    <a href="#">gray</a>
+</div>
+<div class="links-categories" data-flex="vaxis">
+    <a href="#">Articles</a>
+    <a href="#">Illustrator</a>
+    <a href="#">Photoshop Psd</a>
+    <a href="#">Psd Icons</a>
+    <a href="#">Psd Text Effects</a>
+    <a href="#">Uncategorized</a>
+    <a href="#">Vector</a>
+    <a href="#">Website Templates</a>
+</div>
+<div class="links-pagination" data-flex="center w" data-childs="-center center">
+    <a href="#">Previous</a>
+    <a href="#">1</a>
+    <a href="#" class="active">2</a>
+    <a href="#">3</a>
+    <a href="#">4</a>
+    <a href="#">5</a>
+    <a href="#">6</a>
+    <a href="#">Next</a>
+</div>
+<div class="links-social" data-flex="vaxis" data-childs="-center center">
+    <a href="#" class="tw"><i class="icon-tw"></i></a>
+    <a href="#" class="fb"><i class="icon-fb"></i></a>
+    <a href="#" class="rss"><i class="icon-rss"></i></a>
+</div>
