1d897a7fc9b8b99f2aaaa68e70f6c47ad3676b0d
Ivan <auzp@ukr.net>
2016-01-02 19:14:50 +0200

    commit

--- _84layerx/tpls/index.php
+++ _84layerx/tpls/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Index</title>
+        <title>Layerx</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
         <link rel="stylesheet" href="/css/addons/reset.css">
@@ -12,16 +12,149 @@
         <link rel="stylesheet" href="/css/elements.css">  
         <link rel="stylesheet" href="/css/styles.css">  
         <script>
-            var LAYOUT_WIDTH = 960;
-            document.documentElement.style.fontSize = window.innerWidth < LAYOUT_WIDTH ? window.innerWidth/LAYOUT_WIDTH+'px' : '1px';
-            window.onresize = function() {
-                document.documentElement.style.fontSize = window.innerWidth < LAYOUT_WIDTH ? window.innerWidth/LAYOUT_WIDTH+'px' : '1px';
+            var LAYOUT_WIDTH = 1046;
+            document.documentElement.style.fontSize = window.innerWidth < LAYOUT_WIDTH ? window.innerWidth / LAYOUT_WIDTH + 'px' : '1px';
+            window.onresize = function () {
+                document.documentElement.style.fontSize = window.innerWidth < LAYOUT_WIDTH ? window.innerWidth / LAYOUT_WIDTH + 'px' : '1px';
             }
         </script>
     </head>
-    <body>
-        <h1>This is index page</h1>
-        
+    <body data-flex>
+        <div class="side-wrap" data-flex="end">
+            <aside class="sidebar" data-flex="vaxis">
+                <div data-flex="col -center">
+                    <a href="#" id="logo" data-flex="col -center" title="Return to the homepage">
+                        <img src="/images/logo.png" width="122" height="43" alt="Layerx">
+                    </a>
+                    <span>Another psd template by <a href="#">blugraphic.com</a></span>
+                    <form action="#" method="post">
+                        <input type="search" name="search" placeholder="Search the blog...">
+                    </form>
+                    <p>A collection of free design resources including icons, patterns, textures, brushes and more. Free for personal and commercial use.</p>
+                    <h3 class="title">links</h3>
+                    <nav class="links-nav" data-flex="vaxis">
+                        <a href="#">Archives</a>
+                        <a href="#">Talk with us</a>
+                        <a href="#">About us</a>
+                        <a href="#">Portfolio</a>
+                        <a href="#">Services</a>
+                        <a href="#">Clients</a>
+                        <a href="#">Feedback</a>
+                        <a href="#">Latest Posts</a>
+                    </nav>
+                    <h3 class="title">tags</h3>
+                    <div class="links-tags" data-flex="w" data-childs="-center center">
+                        <a href="#">design</a>
+                        <a href="#">blugraphic</a>
+                        <a href="#">art</a>
+                        <a href="#">buttons</a>
+                        <a href="#">website</a>
+                        <a href="#">gray design</a>
+                        <a href="#">templates</a>
+                        <a href="#">graphics</a>
+                        <a href="#">gray</a>
+                    </div>
+                    <h3 class="title">categories</h3>
+                    <div class="links-categories" data-flex="vaxis">
+                        <a href="#">Articles</a>
+                        <a href="#">Illustrator</a>
+                        <a href="#">Photoshop Psd</a>
+                        <a href="#">Psd Icons</a>
+                        <a href="#">Psd Text Effects</a>
+                        <a href="#">Uncategorized</a>
+                        <a href="#">Vector</a>
+                        <a href="#">Website Templates</a>
+                    </div>
+                </div>
+                <span class="copyright visible">All rights reserved 2012<br> visit our website <a href="#">www.blugraphic.com </a> follow us at twitter, <a href="#">@blugraphic</a></span>
+            </aside>
+        </div>
+        <div class="content-wrap">
+            <div data-flex="stripe">
+                <div class="leftbar"></div>
+                <div class="content" data-flex="col -center">
+                    <article>
+                        <figure>
+                            <img src="/images/img1.png" width="565" height="349" alt="">
+                            <div class="overlay">
+                                <div data-flex>
+                                    <a href="#"><i class="icon-add"></i></a>
+                                    <a href="#"><i class="icon-menu"></i></a>
+                                </div>
+                            </div>
+                        </figure>
+                        <h2>Tiam Sem Nisl</h2>
+                        <p>Integer egestas ultricies urna vitae molestie. Donec nec facilisis nisi. Vivamus tempor feugiat velit gravida vehicula. Donec faucibus pellentesque ipsum id varius. Ut rutrum metus sed neque ultricies a dictum ante sagittis. </p>
+                    </article>
+                    <article>
+                        <figure>
+                            <img src="/images/img2.png" width="565" height="349" alt="">
+                            <div class="overlay">
+                                <div data-flex>
+                                    <a href="#"><i class="icon-add"></i></a>
+                                    <a href="#"><i class="icon-menu"></i></a>
+                                </div>
+                            </div>
+                        </figure>
+                        <h2>Tiam Sem Nisl</h2>
+                        <p>Integer egestas ultricies urna vitae molestie. Donec nec facilisis nisi. Vivamus tempor feugiat velit gravida vehicula. Donec faucibus pellentesque ipsum id varius. Ut rutrum metus sed neque ultricies a dictum ante sagittis. </p>
+                    </article>
+                    <article>
+                        <figure>
+                            <img src="/images/img3.png" width="565" height="349" alt="">
+                            <div class="overlay">
+                                <div data-flex>
+                                    <a href="#"><i class="icon-add"></i></a>
+                                    <a href="#"><i class="icon-menu"></i></a>
+                                </div>
+                            </div>
+                        </figure>
+                        <h2>Tiam Sem Nisl</h2>
+                        <p>Integer egestas ultricies urna vitae molestie. Donec nec facilisis nisi. Vivamus tempor feugiat velit gravida vehicula. Donec faucibus pellentesque ipsum id varius. Ut rutrum metus sed neque ultricies a dictum ante sagittis. </p>
+                    </article>
+                    <article>
+                        <figure>
+                            <img src="/images/img4.png" width="565" height="349" alt="">
+                            <div class="overlay">
+                                <div data-flex>
+                                    <a href="#"><i class="icon-add"></i></a>
+                                    <a href="#"><i class="icon-menu"></i></a>
+                                </div>
+                            </div>
+                        </figure>
+                        <h2>Tiam Sem Nisl</h2>
+                        <p>Integer egestas ultricies urna vitae molestie. Donec nec facilisis nisi. Vivamus tempor feugiat velit gravida vehicula. Donec faucibus pellentesque ipsum id varius. Ut rutrum metus sed neque ultricies a dictum ante sagittis. </p>
+                    </article>
+                    <div class="links-pagination" data-flex="center w" data-childs="-center center">
+                        <a href="#">Previous</a>
+                        <a href="#">1</a>
+                        <a href="#" class="active">2</a>
+                        <a href="#">3</a>
+                        <a href="#">4</a>
+                        <a href="#">5</a>
+                        <a href="#">6</a>
+                        <a href="#">Next</a>
+                    </div>
+                    <div class="social hidden" data-flex="-center center">
+                        <span>Social&nbsp;Sharing</span> 
+                        <div class="links-social" data-flex="axis" data-childs="-center center">
+                            <a href="#" class="tw"><i class="icon-tw"></i></a>
+                            <a href="#" class="fb"><i class="icon-fb"></i></a>
+                            <a href="#" class="rss"><i class="icon-rss"></i></a>
+                        </div>                     
+                    </div>
+                    <span class="copyright hidden">All rights reserved 2012<br> visit our website <a href="#">www.blugraphic.com </a> follow us at twitter, <a href="#">@blugraphic</a></span>            
+                </div>
+                <div class="rightbar" data-flex="col -center">
+                    <div class="links-social" data-flex="vaxis" data-childs="-center center">
+                        <a href="#" class="tw"><i class="icon-tw"></i></a>
+                        <a href="#" class="fb"><i class="icon-fb"></i></a>
+                        <a href="#" class="rss"><i class="icon-rss"></i></a>
+                    </div>
+                    <span>Social&nbsp;Sharing</span>
+                </div>
+            </div>
+        </div>
         <script src="/js/jquery/jquery-2.1.4.min.js"></script>
         <script src="/js/tabs-accordions.js"></script>
         <script src="/js/index.js"></script>
