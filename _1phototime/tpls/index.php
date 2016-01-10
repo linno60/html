03f32e41295935523b8aad4819932969e0a6d6f0
Ivan <auzp@ukr.net>
2016-01-10 19:01:47 +0200

    commit

--- _1phototime/tpls/index.php
+++ _1phototime/tpls/index.php
@@ -22,7 +22,7 @@
     <body>
         <header>
             <div class="lwrap" data-flex="vaxis">
-                <nav class="links-nav" data-flex="-center">
+                <nav class="links-nav" data-flex="-center around" data-flex-480="-center around w">
                     <a href="#">home</a>
                     <a href="#">photoapp</a>
                     <figure><img src="/images/icons/camera.png"></figure>
@@ -30,7 +30,7 @@
                     <a href="#">download</a>
                 </nav>
                 <div data-flex="vaxis">
-                    <h1>your life, a photo</h1>
+                    <h1>your life , a photo</h1>
                     <p>print your life in a simple photo</p>
                     <a href="#" class="btn btn-default">get started</a>
                 </div>
@@ -42,7 +42,7 @@
             </div>   
         </header>
         <section class="article-wrap photo">
-            <div class="lwrap" data-flex>
+            <div class="lwrap" data-flex="stripe" data-flex-640="vstripe">
                 <article>
                     <h2>rappresent your life with a simple photo</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -52,11 +52,13 @@
                         ut aliquip ex ea commodo consequat.</p>
                     <a href="#" class="btn btn-default">get started</a>
                 </article>
-                <figure><img src="/images/img1.png"></figure>
+                <div data-flex="-end">
+                    <figure><img src="/images/img1.png"></figure>  
+                </div>
             </div>
         </section>
         <section class="article-wrap">
-            <div class="lwrap" data-flex>
+            <div class="lwrap" data-flex="stripe r" data-flex-640="vstripe w">
                 <article>
                     <h2>design</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -71,11 +73,13 @@
                         <a href="#"><i class="icon-right"></i></a>
                     </div>
                 </article>
-                <figure><img src="/images/img2.png"> </figure>  
+                <div data-flex="-end">
+                    <figure><img src="/images/img2.png"></figure>  
+                </div> 
             </div>
         </section>
         <section class="article-wrap community">
-            <div class="lwrap" data-flex>
+            <div class="lwrap" data-flex="axis">
                 <article data-flex="vaxis">
                     <figure><img src="/images/icons/camerag.png"></figure>
                     <h2>our community</h2>
@@ -86,33 +90,35 @@
                         <a href="#"><i class="icon-left"></i></a>
                         <a href="#"><i class="icon-right"></i></a>
                     </div>
-                    <div data-flex>
+                    <div data-flex="stripe" data-flex-640="vstripe">
                         <figure><img src="images/img3.png"></figure>
                         <figure><img src="images/img4.png"></figure>
                     </div>
                 </div>
             </div>
         </section>
-        <section class="subscribe" data-flex="col -center center">
-            <h2>subscribe tous community</h2>
-            <form action="#" method="post" data-flex>
-                <input type="email" name="email" placeholder="your email">
-                <button class="btn btn-check"><i class="icon-check"></i></button>
-            </form>
+        <section class="subscribe">
+            <div class="lwrap" data-flex="col -center center">
+                <h2>subscribe tous community</h2>
+                <form action="#" method="post" data-flex>
+                    <input type="email" name="email" placeholder="your email">
+                    <button class="btn btn-check"><i class="icon-check"></i></button>
+                </form>
+            </div>
         </section>
         <section class="download">
             <div class="lwrap" data-flex="vaxis">
                 <h2>download it</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <div>
-                    <a href="#" class="btn btn-alt" data-childs="-center center"><em>apple store</em><span><i class="icon-apple"></i></span></a>
-                    <a href="#" class="btn btn-alt" data-childs="-center center"><em>play store</em><span><i class="icon-android"></i></span></a>
+                <div data-flex-640="vaxis">
+                    <a href="#" class="btn btn-alt" data-childs="-center"><em>apple store</em><span><i class="icon-apple"></i></span></a>
+                    <a href="#" class="btn btn-alt" data-childs="-center"><em>play store</em><span><i class="icon-android"></i></span></a>
                 </div>
             </div>
         </section>
         <footer class="lwrap" data-flex="axis">
             <div>
-                <nav class="links-footer" data-flex="-center">
+                <nav class="links-footer" data-flex="-center w">
                     <a href="#">credits</a>|
                     <a href="#">privacy</a>|
                     <a href="#">our teams</a>
