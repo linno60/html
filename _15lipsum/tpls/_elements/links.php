7cc8d8adf17144d71cfffcca2dcd42842d7ce11b
Ivan <auzp@ukr.net>
2016-01-21 22:20:25 +0200

    commit

---
+++ _15lipsum/tpls/_elements/links.php
@@ -0,0 +1,14 @@
+<nav class="links-nav" data-flex="-center w">
+    <a href="#">home</a>
+    <a href="#">products</a>
+    <a href="#">news</a>
+    <a href="#">about us</a>
+    <a href="#">contact us</a>
+</nav>
+<nav class="links-footer" data-flex="-center center w">
+    <a href="#">home</a>|
+    <a href="#">products</a>|
+    <a href="#">news</a>|
+    <a href="#">about us</a>|
+    <a href="#">contact us</a>
+</nav>
