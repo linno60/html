f5957973abecec7d0ad3117242e9604645ac673d
Ivan <auzp@ukr.net>
2016-01-26 17:47:25 +0200

    commit

--- _20redux/tpls/index.php
+++ _20redux/tpls/index.php
@@ -21,15 +21,15 @@
     </head>
     <body>
         <header>
-            <div class="lwrap">
-                <div class='nav' data-flex='axis'>
-                    <div data-flex="axis">
+            <div class="lwrap" data-flex='vaxis'>
+                <div data-flex='axis' data-flex-480='vaxis'>
+                    <div data-flex="-center w">
                         <a href="/" title="Return to the homepage" id="logo">
                             <img src="/images/logo.png" width="96" height="32" alt="Redux">
                         </a>
                         <small>|&ensp;Super-Dooper Business</small>
                     </div>
-                    <nav class="links-nav" data-flex="end w" data-childs="-center center">
+                    <nav class="links-nav" data-flex="end w" data-childs="-center center" data-flex-480='center w'>
                         <a href='#'>Features<i class='icon-arrow'></i></a>
                         <a href='#'>Templates<i class='icon-arrow'></i></a>
                         <a href='#'>Shortcodes</a>
@@ -48,10 +48,11 @@
                         <figure><img src="/images/right.png"></figure>
                     </div>
                     <a href="#"><i class="icon-next"></i></a>
-                </div>                       
+                </div>  
+            </div>
         </header>
         <section class='feature'>
-            <div class='lwrap' data-flex='stripe'>
+            <div class='lwrap' data-flex='stripe' data-flex-480='vstripe'>
                 <div>
                     <article class='clearfix'>
                         <figure>
@@ -98,7 +99,7 @@
         </section>
         <section class='content'>
             <div class='lwrap'>
-                <div class='about' data-flex='stripe'>
+                <div class='about' data-flex='stripe w' data-flex-480='vstripe'>
                     <div>
                         <h5>Endlessly configurable content.</h5>
                         <p>Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
@@ -129,14 +130,14 @@
                         <span class='caption' data-flex='-center center'>Overview of options and widgets - 2 mins 33 secs.</span>
                     </div>
                 </div>
-                <div class='callout' data-flex='axis'>
-                    <div>
-                        <h2>Join the cool kids and get your copy of Redux now...</h2>
+                <div class='callout' data-flex='axis' data-flex-480='vaxis'>
+                    <div data-flex-480='vaxis'>
+                        <h2>Join the cool kids and get your copy of Redux now....</h2>
                         <p>Redux is avalaible for the ridiculously low, one-time cost of $37! That's right, I'm practically giving it away!</p>
                     </div>
-                    <a href='#' class='btn btn-default'>Yes, I want this theme now!</a>  
+                    <div class='wrap'><a href='#' class='btn btn-default'>Yes, I want this theme now!</a></div>  
                 </div> 
-                <div class='blog' data-flex='stripe'>
+                <div class='blog' data-flex='stripe' data-flex-480='vstripe'>
                     <div>
                         <h5>Ramblings from the Redux Blog.</h5>
                         <article class='clearfix'>
@@ -166,31 +167,63 @@
                     </div>
                     <div>
                         <h5>Musings in 140 characters or less.</h5>
-                        <p><a href='#'>@JamesBliss</a> Hard to get get a balance for those that know their way around WP/HTML/CSS and those that don't I find <time>33 minutes ago</time></p>
-                        <p><a href='#'>@JamesBliss</a> Hard to get get a balance for those that know their way around WP/HTML/CSS and those that don't I find <time>33 minutes ago</time></p>
-                        <p><a href='#'>@JamesBliss</a> Hard to get get a balance for those that know their way around WP/HTML/CSS and those that don't I find <time>33 minutes ago</time></p>
-                        <p><a href='#'>@JamesBliss</a> Hard to get get a balance for those that know their way around WP/HTML/CSS and those that don't I find <time>33 minutes ago</time></p>                        
+                        <p><a href='#'>@JamesBliss</a> Hard to get get a balance for those that know their way around WP/HTML/CSS and those that don't I find <time>&nbsp;33 minutes ago</time></p>
+                        <p><a href='#'>@JamesBliss</a> Hard to get get a balance for those that know their way around WP/HTML/CSS and those that don't I find <time>&nbsp;33 minutes ago</time></p>
+                        <p><a href='#'>@JamesBliss</a> Hard to get get a balance for those that know their way around WP/HTML/CSS and those that don't I find <time>&nbsp;33 minutes ago</time></p>
+                        <p><a href='#'>@JamesBliss</a> Hard to get get a balance for those that know their way around WP/HTML/CSS and those that don't I find <time>&nbsp;33 minutes ago</time></p>                        
                     </div>
                     <div>
                         <h5>What customers are sayin'.</h5>  
-                        <article class="clearfix">
+                        <blockquote class="clearfix">
                             <figure><img src="images/img4.png"></figure>
-                            <h3>Marcin Gora</h3>
-                            <span>Managing Director Apple UK</span>
-                            <a href='#'>Apple.co.uk</a>
-                            <blockquote>"Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet..."</blockquote>                        
-                        </article>
-                        <article class="clearfix">
+                            <cite data-flex="col -start">
+                                <strong>Marcin Gora</strong>
+                                <span>Managing Director Apple UK</span>
+                                <a href='#'>Apple.co.uk</a>
+                            </cite>
+                            <q>"Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet..."</q>                        
+                        </blockquote>
+                        <blockquote class="clearfix">
                             <figure><img src="images/img4.png"></figure>
-                            <h3>Steven Bluck</h3>
-                            <span>Technical Director Audi International</span>
-                            <a href='#'>Audi.com</a>
-                        </article>
-                        <blockquote>"Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet..."</blockquote>                        
+                            <cite data-flex="col -start">
+                                <strong>Steven Bluck</strong>
+                                <span>Technical Director Audi International</span>
+                                <a href='#'>Audi.com</a>
+                            </cite>
+                            <q>"Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet..."</q>      
+                        </blockquote>                    
                     </div>
                 </div>
             </div>
         </section>
+        <footer>
+            <section class='footer'>
+                <div class='lwrap' data-flex="stripe" data-flex-480='vstripe'>
+                    <div>
+                        <h4>If you're using Redux then we want to hear from you!</h4>
+                        <p>Sed ornare dictum mi, sed rutrum metus condimentum sodales. Fusce quis tellus mollis sapien porta placerat. In dapibus velit eget felis rutrum posuere. Pellentesque tincidunt augue vitae diam auctor id fermentum ante elementum. estibulum in luctus urna. Vivamus euismod, ligula eu commodo accumsan.</p>
+                        <div>
+                            <span>Email: <a href='mailto:hello@redux.com'>hello@redux.com</a></span>
+                            <span>Tel: 1-408-996-1010</span>
+                        </div>
+                    </div>
+                    <div>
+                        <h4>Subscribe and get the inside scoop.</h4>
+                        <p>Subscribe to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
+                        <form class="form-subscribe" action='#' method='post' data-flex='axis'>
+                            <input type='email' name="input" placeholder='enter your email address'>
+                            <button><span class="btn btn-subscribe">subscribe</span></button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+            <section class='copyright'>
+                <div class='lwrap' data-flex='axis w'>
+                    <span>Copyright 2010 <a href='#'>Redux Theme</a>. All Rights Reserved.</span>
+                    <span>Powered by <a href='#'>WordPress</a>. <a href='#'>Redux Theme</a> by <a href='#'>Orman</a></span>
+                </div>
+            </section>
+        </footer>
         <script src="/js/jquery/jquery-2.1.4.min.js"></script>
         <script src="/js/tabs-accordions.js"></script>
         <script src="/js/index.js"></script>
