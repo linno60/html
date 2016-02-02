7f286ba09a24cfabfe38574de32c708203984cd6
Ivan <auzp@ukr.net>
2016-02-02 21:21:35 +0200

    commit

---
+++ _28blog/tpls/_elements/links.php
@@ -0,0 +1,23 @@
+<div class="links-social" data-flex="end w" data-childs="-center center">
+    <a href="#" class="rss"><i class="icon-rss"></i></a>
+    <a href="#" class="fb"><i class="icon-fb"></i></a>
+    <a href="#" class="tw"><i class="icon-tw"></i></a>
+</div>
+<div class="links-nav" data-flex="w" data-childs="-center center">
+    <a href="#">home</a>
+    <a href="#">cinema</a>
+    <a href="#">magazine</a>
+    <a href="#">cssauthor.com</a>
+    <a href="#">topmenu</a>
+    <a href="#">archives</a>
+    <a href="#">contact us</a>
+</div>
+<ul class="links-aside" data-childs="-start">
+    <li><a href="#">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </a></li>
+    <li><a href="#">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</a></li>
+    <li><a href="#">When an unknown printer took a galley of type and scrambled it to make a type specimen book. </a></li>
+    <li><a href="#">It has survived not only five centuries, but also the leap into electronic typesetting</a></li>
+    <li><a href="#">Remaining essentially unchanged. It was popular&shy;ised in the 1960s with the release of</a></li>
+    <li><a href="#">Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing </a></li>
+    <li><a href="#">Software like Aldus PageMaker including versions of Lorem Ipsum.</a></li>
+</ul>
---
+++ _28blog/tpls/_elements/other.php
@@ -0,0 +1,2 @@
+<h2 class="hdr-alt" data-flex>top stories</h2> 
+<h2 class="hdr" data-flex>archives</h2>
