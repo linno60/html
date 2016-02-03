7c0cb07758cec07f56a7475ffe8e9a406b8a85c8
Ivan <auzp@ukr.net>
2016-02-03 19:44:36 +0200

    commit

---
+++ _29creativemass/tpls/_elements/links.php
@@ -0,0 +1,8 @@
+<div class="social" data-flex="row--between">
+    <a href="#"><i class="icon-rss"></i></a>
+    <a href="#"><i class="icon-tw"></i></a>
+    <a href="#"><i class="icon-fb"></i></a>
+    <a href="#"><i class="icon-in"></i></a>
+    <a href="#"><i class="icon-sk"></i></a>
+    <a href="#"><i class="icon-yt"></i></a>
+</div>
