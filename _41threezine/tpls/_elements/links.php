5b84359d049929a59c272f19b4d4724df1c1190b
Ivan <auzp@ukr.net>
2016-02-13 12:35:44 +0200

    commit

---
+++ _41threezine/tpls/_elements/links.php
@@ -0,0 +1,57 @@
+<nav class="links-top" data-flex="w">
+    <a href="#">Home</a>
+    <a href="#">About</a>
+    <a href="#">Terms of Use</a>
+    <a href="#">Contact</a>
+</nav>
+<div class="links-social" data-flex="end w">
+    <a href="#"><i class="icon-fb"></i></a>
+    <a href="#"><i class="icon-del"></i></a>
+    <a href="#"><i class="icon-fl"></i></a>
+    <a href="#"><i class="icon-ff"></i></a>
+    <a href="#"><i class="icon-tb"></i></a>
+    <a href="#"><i class="icon-tw"></i></a>
+</div>
+<nav class="links-nav" data-flex="w">
+    <a href="#">home</a>
+    <a href="#">graphic design</a>
+    <a href="#">tutorials</a>
+    <a href="#">freebies</a>
+    <a href="#">psd templates</a>
+    <a href="#">photography</a>
+</nav>
+<div class="links-tags" data-childs="-center center" data-flex="w">
+    <a href="#">Photography</a>
+    <a href="#">Web Design</a>
+    <a href="#">Drawings</a>
+    <a href="#">Coding</a>
+    <a href="#">Vector</a>
+    <a href="#">Manipulation</a>
+    <a href="#">Textures</a>
+    <a href="#">Print</a>
+    <a href="#">Animation</a>
+    <a href="#">Black & White</a>
+    <a href="#">Digital Art</a>
+    <a href="#">Graphic Design</a>
+</div>
+<div class="links-archive" data-flex="col -start">
+    <a href="#">October 2010</a>
+    <a href="#">September 2010</a>
+    <a href="#">August 2010</a>
+    <a href="#">July 2010</a>
+    <a href="#">June 2010</a>
+    <a href="#">May 2010</a>
+    <a href="#">April 2010</a>
+    <a href="#">March 2010</a>  
+</div>
+<div class="links-meta" data-flex="w" data-childs="-center">
+    <a href="#"><i class="icon-calendar"></i>21 Oct 2010</a>
+    <a href="#"><i class="icon-user"></i>by admin</a>
+    <a href="#"><i class="icon-cloud"></i>12 Comments</a>
+    <a href="#"><i class="icon-link"></i>Permalink</a>
+</div>
+<div class="links-tag" data-flex="-center w">
+    <i class="icon-mark"></i>
+    <a href="#">Graphic Design</a>, 
+    <a href="#">Web Design</a>
+</div>
---
+++ _41threezine/tpls/_elements/other.php
@@ -0,0 +1,1 @@
+<h3 class="hdr">search</h3>
