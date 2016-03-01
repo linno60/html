e0b266f2e00948a1156af5a566a08895f9e744a8
Ivan <auzp@ukr.net>
2016-03-01 22:26:39 +0200

    commit

---
+++ _61v2portfolio/tpls/_elements/buttons.php
@@ -0,0 +1,5 @@
+<a href="#" class="btn btn-slide prev"></a>
+<a href="#" class="btn btn-slide next"></a>
+<a href="#" class="btn btn-slide alt prev"></a>
+<a href="#" class="btn btn-slide alt next"></a>
+<a href="#" class="btn btn-default"><span></span><em>Request A Quote</em></a>
---
+++ _61v2portfolio/tpls/_elements/lists.php
@@ -0,0 +1,6 @@
+<ul class="list-default" data-childs>
+    <li>Donec feugiat lectus vel dolor venenatis condimentum.</li>
+    <li>Cras pharetra dignissim tortor at ullamcorper.</li>
+    <li>Duis id viverra sem. Integer commodo adipiscing elit vel sodales.</li>
+    <li>Aliquam bibendum tempor ligula, ac cursus mauris sodales sit amet.</li>
+</ul>
