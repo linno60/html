3349c302e3663ede9e648c0c4d991ba896b5cad2
Ivan <auzp@ukr.net>
2016-03-10 20:57:30 +0200

    commit

--- _67daylite/tpls/_elements/links.php
+++ _67daylite/tpls/_elements/links.php
@@ -13,7 +13,7 @@
     <a href="#" class="pin"><i class="icon-pin"></i></a>
 </div>
 <div class="links-post" data-flex="w" data-childs="-center">
-    <a href="#">Photography, journal</a>|
+    <a href="#">Photography</a>, <a href="#">journal</a>|
     <a href="#">9 Comments</a>|
     <a href="#" data-flex="row-center">21 <i class="icon-like"></i></a>
 </div>
---
+++ _67daylite/tpls/_elements/lists.php
@@ -0,0 +1,5 @@
+<ul class="list-default" data-childs>
+    <li>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</li>
+    <li>Eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare .</li>
+    <li>Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</li>
+</ul>
