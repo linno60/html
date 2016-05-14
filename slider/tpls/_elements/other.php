e5750f77652c082b1cbaddff5f1c4ea6ff8a67c6
Ivan <auzp@ukr.net>
2016-05-14 21:37:03 +0300

    commit

--- slider/tpls/_elements/other.php
+++ slider/tpls/_elements/other.php
@@ -1,7 +1,81 @@
-<div class="wrapper"><div class="item"></div></div>
-<div class="gate">
-    <big class="first"><span>First</span></big>
-    <big class="second"><span>Second</span></big>
+<div class="wrap" data-flex="w">
+    <div class="wrapper"><div class="item"></div></div>
+    <div class="gate">
+        <big class="first"><span>First</span></big>
+        <big class="second"><span>Second</span></big>
+    </div>
+    <div class="rising"><div></div></div>
+    <div class="sun"></div>
+    <div class="moon"></div>
 </div>
-<div class="rising"><div></div></div>
-<div class="sun"></div>
+
+<div class="animated"><span class="bounce">bounce</span></div>
+<div class="animated"><span class="flash">flash</span></div>
+<div class="animated"><span class="pulse">pulse</span></div>
+<div class="animated"><span class="rubberBand">rubberBand</span></div>
+<div class="animated"><span class="shake">shake</span></div>
+<div class="animated"><span class="headShake">headShake</span></div>
+<div class="animated"><span class="swing">swing</span></div>
+<div class="animated"><span class="tada">tada</span></div>
+<div class="animated"><span class="wobble">wobble</span></div>
+<div class="animated"><span class="jello">jello</span></div>
+<div class="animated"><span class="bounceIn">bounceIn</span></div>
+<div class="animated"><span class="bounceInDown">bounceInDown</span></div>
+<div class="animated"><span class="bounceInLeft">bounceInLeft</span></div>
+<div class="animated"><span class="bounceInRight">bounceInRight</span></div>
+<div class="animated"><span class="bounceInUp">bounceInUp</span></div>
+<div class="animated"><span class="bounceOut">bounceOut</span></div>
+<div class="animated"><span class="bounceOutDown">bounceOutDown</span></div>
+<div class="animated"><span class="bounceOutLeft">bounceOutleft</span></div>
+<div class="animated"><span class="bounceOutRight">bounceOutRight</span></div>
+<div class="animated"><span class="bounceOutUp">bounceOutUp</span></div>
+<div class="animated"><span class="fadeIn">fadeIn</span></div>
+<div class="animated"><span class="fadeInDown">fadeInDown</span></div>
+<div class="animated"><span class="fadeInDownBig">fadeInDownBig</span></div>
+<div class="animated"><span class="fadeInLeft">fadeInLeft</span></div>
+<div class="animated"><span class="fadeInLeftBig">fadeInLeftBig</span></div>
+<div class="animated"><span class="fadeInRight">fadeInRight</span></div>
+<div class="animated"><span class="fadeInRightBig">fadeInRightBig</span></div>
+<div class="animated"><span class="fadeInUp">fadeInUp</span></div>
+<div class="animated"><span class="fadeInUpBig">fadeInUpBig</span></div>
+<div class="animated"><span class="fadeOut">fadeOut</span></div>
+<div class="animated"><span class="fadeOutDown">fadeOutDown</span></div>
+<div class="animated"><span class="fadeOutDownBig">fadeOutDownBig</span></div>
+<div class="animated"><span class="fadeOutLeft">fadeOutLeft</span></div>
+<div class="animated"><span class="fadeOutLeftBig">fadeOutLeftBig</span></div>
+<div class="animated"><span class="fadeOutRight">fadeOutRight</span></div>
+<div class="animated"><span class="fadeOutRightBig">fadeOutRightBig</span></div>
+<div class="animated"><span class="fadeOutUp">fadeOutUp</span></div>
+<div class="animated"><span class="fadeOutUpBig">fadeOutUpBig</span></div>
+<div class="animated"><span class="flip">flip</span></div>
+<div class="animated"><span class="flipInX">flipInX</span></div>
+<div class="animated"><span class="flipInY">flipInY</span></div>
+<div class="animated"><span class="flipOutX">flipOutX</span></div>
+<div class="animated"><span class="flipOutY">flipOutY</span></div>
+<div class="animated"><span class="lightSpeedIn">lightSpeedIn</span></div>
+<div class="animated"><span class="lightSpeedOut">lightSpeedOut</span></div>
+<div class="animated"><span class="rotateIn">rotateIn</span></div>
+<div class="animated"><span class="rotateInDownLeft">rotateInDownLeft</span></div>
+<div class="animated"><span class="rotateInDownRight">rotateInDownRight</span></div>
+<div class="animated"><span class="rotateInUpLeft">rotateInUpLeft</span></div>
+<div class="animated"><span class="rotateInUpRight">rotateInUpRight</span></div>
+<div class="animated"><span class="rotateOutDownLeft">rotateOutDownLeft</span></div>
+<div class="animated"><span class="rotateOutDownRight">rotateOutDownRight</span></div>
+<div class="animated"><span class="rotateOutUpLeft">rotateOutUpLeft</span></div>
+<div class="animated"><span class="rotateOutUpRight">rotateOutUpRight</span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
+<div class="animated"><span class=""></span></div>
