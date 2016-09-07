db73fdf063c8fac3bed6471edf4bf77dcf70c79a
Ivan <auzp@ukr.net>
2016-09-07 11:25:42 +0300

    commit

--- _124developer/tpls/index.php
+++ _124developer/tpls/index.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="/css/elements.css">  
         <link rel="stylesheet" href="/css/styles.css">  
         <script>
-            var LAYOUT_WIDTH = 1000;
+            var LAYOUT_WIDTH = 982;
             document.documentElement.style.fontSize = window.innerWidth < LAYOUT_WIDTH ? window.innerWidth / LAYOUT_WIDTH + 'px' : '1px';
             window.onresize = function () {
                 document.documentElement.style.fontSize = window.innerWidth < LAYOUT_WIDTH ? window.innerWidth / LAYOUT_WIDTH + 'px' : '1px';
@@ -21,8 +21,9 @@
     </head>
     <body>
         <div class="lwrap">
-            <div class="wrap" data-flex="stripe w">
-                <div>
+            <a href="#" id="top"></a>
+            <div class="wrap" data-flex="stripe w" data-flex-640="vstripe r">
+                <div data-flex="stripe w" data-flex-480="vstripe">
                     <h1 class="title"><strong>adrian olivier</strong>web developer</h1>
                     <section>
                         <h2 class="title">about me</h2>
@@ -59,8 +60,8 @@
                         <div data-flex="stripe w">
                             <div>
                                 <span class="lbl lbl-default"><i class="icon-mail"></i></span>
-                                <span>Email Address:</span>
-                                <a href="mailto:contact@adrianolivier.com">contact@adrianolivier.com</a>
+                                <span>Emaill Address:</span>
+                                <a href="mailto:contact@adrianolivier.com">contact@adrian<wbr>olivier.com</a>
                             </div>
                             <div>
                                 <span class="lbl lbl-default"><i class="icon-skype"></i></span>
@@ -70,56 +71,57 @@
                             <div>
                                 <span class="lbl lbl-default"><i class="icon-address"></i></span>
                                 <span>Address:</span>
-                                <address>Poland, 30-229 Cracow, ul.Zielona 2</address>
+                                <address>Poland, 30-229 Cracow,<br> ul. Zielona 2</address>
                             </div>
                             <div>
                                 <span class="lbl lbl-default"><i class="icon-phone"></i></span>
                                 <span>Telephone:</span>
-                                <a href="#">+480 999 99 00</a>
+                                <a href="tel:+48 999 99 00">+48 999 99 00</a>
                             </div>
                         </div>
                     </section>
                 </div>
-                <div>
+                <div data-flex-640="stripe w" data-flex-480="vstripe">
+                    <h1 class="title hidden"><strong>adrian olivier</strong>web developer</h1>
                     <nav class="links-nav" data-childs="center -center" data-flex="end w">
-                        <a href="#" class="active">home</a>
-                        <a href="#">work</a>
-                        <a href="#">get in touch</a>
-                        <a href="#">skills</a>
+                        <a href="#" class="active"><span>home</span></a>
+                        <a href="#"><span>work</span></a>
+                        <a href="#"><span>get in touch</span></a>
+                        <a href="#"><span>skills</span></a>
                     </nav>
                     <section>
                         <h2 class="title">welcome</h2>
                         <p>Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada ultricies. Curabitur et ligula. </p>
                         <p>Ut molestie a, ultricies porta urna. Vestibulum commodo volutpat a, convallis ac, laoreet enim. Phasellus fermentum in, dolor. Pellentesque facilisis. <br>Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit</p>
                     </section>
-                    <section class='slider'>
+                    <section class='slider' data-flex="axis">
                         <input name="anchor1" id='slide1-0' type="radio">
                         <input name="anchor1" id='slide1-1' type="radio">
                         <input name="anchor1" id='slide1-2' type="radio">
                         <input name="anchor1" id='play1' type="radio" checked>
-                        <ul>
-                            <li><img src="/images/almost.png" width="443" height="460" alt=""></li>
-                            <li class='num0'><img src="/images/almost.png" width="443" height="460" alt=""><a href="#" class="overlay"></a></li>
-                            <li class='num1'><img src="/images/slider2.jpg" width="443" height="460" alt=""><a href="#" class="overlay"></a></li>
-                            <li class='num2'><img src="/images/slider3.jpg" width="443" height="460" alt=""><a href="#" class="overlay"></a></li>
-                        </ul>
                         <div class='arrowprev'>
                             <label class='num0' for='slide1-0'></label>
                             <label class='num1' for='slide1-1'></label>
                             <label class='num2' for='slide1-2'></label>
                         </div>
+                        <ul>
+                            <li><img src="/images/almost.png" width="443" height="460" alt=""></li>
+                            <li class='num0'><img src="/images/almost.png" width="443" height="460" alt=""><a href="#" class="overlay"><span></span></a></li>
+                            <li class='num1'><img src="/images/slider2.jpg" width="443" height="460" alt=""><a href="#" class="overlay"><span></span></a></li>
+                            <li class='num2'><img src="/images/slider3.jpg" width="443" height="460" alt=""><a href="#" class="overlay"><span></span></a></li>
+                        </ul>
                         <div class='arrownext'>
                             <label class='num0' for='slide1-0'></label>
                             <label class='num1' for='slide1-1'></label>
                             <label class='num2' for='slide1-2'></label>
                         </div>
                     </section>
-                    <section>
+                    <section class="skills-wrap">
                         <hgroup>
                             <h2 class="title">skills</h2>
                             <h4 class="subtitle">technical skills</h4>
                         </hgroup>
-                        <div class="skills" data-flex="between -end" data-childs="-center">
+                        <div class="skills" data-flex="between -end">
                             <div><span>php</span></div>
                             <div><span>asp/perl</span></div>
                             <div><span>seo</span></div>
@@ -129,9 +131,9 @@
                             <div><span>wordpress</span></div>
                         </div>
                     </section>
-                    <section>
+                    <section class="technology">
                         <h4 class="subtitle">technology</h4>
-                        <div data-flex="axis w">
+                        <div data-flex="axis w" data-flex-960="around -center w">
                             <a href="#"><i class="icon-wp"></i></a>
                             <a href="#"><i class="icon-joomla"></i></a>
                             <a href="#"><i class="icon-jquery"></i></a>
@@ -142,8 +144,8 @@
                 </div>
             </div> 
             <div>
-                <section data-flex="vaxis" class="services">                    
-                    <div data-flex="center">
+                <section class="services">                    
+                    <div data-flex="center -center">
                         <figure><img src="/images/photo.png" width="181" height="200" alt=""></figure>
                         <div>
                             <h2 class="title">how can i help?</h2>
@@ -156,6 +158,9 @@
                                 <li>Delivering Email Campaigns</li>
                             </ul>
                         </div>
+                    </div>
+                    <div data-flex="end">
+                        <a href="#top" class="btn btn-default anchor">top</a>
                     </div>
                 </section>
                 <footer>Design by Webvilla.pl 2012</footer>
