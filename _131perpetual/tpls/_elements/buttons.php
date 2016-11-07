5f86e8a2e90b258b295ab93736f1c3fc1a919824
Ivan <auzp@ukr.net>
2016-11-07 19:51:33 +0200

    commit

--- _131perpetual/tpls/_elements/buttons.php
+++ _131perpetual/tpls/_elements/buttons.php
@@ -1,3 +1,4 @@
 <a href="#" class="btn btn-default">read more</a>
 <a href="#" class="btn btn-alt">read more</a>
 <button><span class="btn btn-submit">Submit</span></button>
+<a href="#" class="btn btn-alt lg">read more</a>
--- _131perpetual/tpls/index.php
+++ _131perpetual/tpls/index.php
@@ -8,8 +8,9 @@
         <link rel="stylesheet" href="/css/addons/reset-elements.css">
         <link rel="stylesheet" href="/css/addons/data-flex.css">
         <link rel="stylesheet" href="/css/addons/data-tabs.css">
-        <link rel="stylesheet" href="/css/addons/generic.css">  
-        <link rel="stylesheet" href="/css/elements.css">  
+        <link rel="stylesheet" href="/css/addons/generic.css"> 
+        <link rel="stylesheet" href="/js/jquery/plugins/jQuery-Feature-Carousel/css/feature-carousel.css">
+        <link rel="stylesheet" href="/css/elements.css">         
         <link rel="stylesheet" href="/css/styles.css">  
         <script>
             var LAYOUT_WIDTH = 960;
@@ -40,22 +41,37 @@
             <section class="slider-wrap">
                 <div class="lwrap" data-flex="stripe">
                     <div class="slider">
-
+                        <div id="carousel">
+                            <div class="carousel-feature">
+                                <a href="#"><img class="carousel-image" src="/images/terion.png" width="521" height="343" alt=""></a>
+                            </div>
+                            <div class="carousel-feature">
+                                <a href="#"><img class="carousel-image" src="/images/vivaro.png" width="260" height="308" alt=""></a>
+                            </div>
+                            <div class="carousel-feature">
+                                <a href="#"><img class="carousel-image" src="/images/terion.png" width="521" height="343" alt=""></a>
+                            </div>
+                            <div class="carousel-feature">
+                                <a href="#"><img class="carousel-image" src="/images/vivaro.png" width="260" height="308" alt=""></a>
+                            </div>
+                        </div>
                     </div>
-                    <article>
+                    <article data-flex="col -center">
                         <h1>web design & more</h1>
                         <p>Sed gravida nunc at erat tincidunt iaculis. Nam ac orci diam, sit amet commodo leo. Phasellus consequat augue sed arcu volutpat lobortis.</p>
                         <p>Duis a quam ut neque mollis imperdiet a a lacus. Integer quis diam nunc, eu aliquet nibh.</p>
+                        <a href="#" class="btn btn-alt lg">read more</a>
                     </article>
                 </div>
             </section>
             <section class="preview">
-                <div class="lwrap" data-flex="stripe">
+                <div class="lwrap" data-flex="axis">
                     <div>
                         <h3>lorem ipsum dolor sit amet neque</h3>
                         <p>Sed gravida nunc at erat tincidunt iaculis. Nam ac orci amet commodo leo. Phasellus consequat augue sed.</p> 
                         <a href="#" class="btn btn-default">read more</a>
                     </div>
+                    <span class="divider">{}</span>
                     <div>
                         <h3>commodo phaellus arcu sed nibh</h3>
                         <p>Sed gravida nunc at erat tincidunt iaculis. Nam ac orci amet commodo leo. Phasellus consequat augue sed.</p> 
@@ -63,51 +79,60 @@
                     </div>
                 </div>
             </section>
-            <section class="content-wrap">
+            <main class="content-wrap">
                 <div class="lwrap" data-childs="stripe">
-                    <div>
+                    <div class="intro">
                         <article>
                             <h2 data-flex="-center"><i class="icon-coffee"></i>odio mollus cursus penant maecasus</h2>
                             <p>Aenean eget rutrum sapien. Phasellus gravida nibh leo, sed varius quam. Duis sed diam nisi. Donec urna eros, fermentum at tincidunt nec, molestie quis neque. Praesent aliquam dignissim orci, quis ullamcorper leo eleifend nec. Mauris pellentesque, enim eget consectetur pharetra, magna mi cursus tortor, non luctus leo mi ac tellus. Proin magna lorem, adipiscing sit amet semper placerat, cursus ut nunc. Integer pellentesque lobortis congue.</p>
                             <a href="#" class="btn btn-alt">read more</a>
                         </article>
                         <div class="accordion" data-childs="-center">
-                            <a href="#" class="active">ansen quist diam nibh lacus</a>
-                            <div class="active">Tab number 1</div>
+                            <a href="#">ansen quist diam nibh lacus</a>
+                            <div>Tab number 1</div>
                             <a href="#">erat alium cras orci magnus nam sit</a>
                             <div>Tab number 2</div>
-                            <a href="#">ligula aeanes quist</a>
-                            <div>Tab number 3</div>
+                            <a href="#" class="active">ligula aeanes quist</a>
+                            <div class="active"></div>
                             <a href="#">odio mollus cursus penant maecasus</a>
                             <div>Tab number 4</div>
                         </div>
                     </div>
                     <div class="featured">
-                        <figure><img src="/images/featured.png" width="302" height="151" alt=""></figure>
-                        <figure><img src="/images/featured.png" width="302" height="151" alt=""></figure>
-                        <figure><img src="/images/featured.png" width="302" height="151" alt=""></figure>
+                        <article>
+                            <figure><img src="/images/featured.png" width="302" height="151" alt=""></figure>
+                            <p>Proin magna lorem, adipiscing sit amet semper placerat, cursus ut nunc. Integer pellentesque.</p>
+                        </article>
+                        <article>
+                            <figure><img src="/images/featured.png" width="302" height="151" alt=""></figure>
+                            <p> Integer pellentesque. Proin magna lorem, adipiscing sit amet semper placerat, cursus ut</p>
+                        </article>
+                        <article>
+                            <figure><img src="/images/featured.png" width="302" height="151" alt=""></figure>
+                            <p>Adipiscing sit amet semper placerat, cursus ut nunc. Integer pellentesque. Phaeret eget unt.</p>
+                        </article>
                     </div>
                 </div>
-            </section>
+            </main>
             <section class="news">
                 <div class="lwrap" data-flex="vaxis">
                     <h2 class="hdr">Latest News</h2>
                     <div data-flex="stripe w">
-                      <article>
-                        <h4 data-flex="-center"><i class="icon-download"></i>torquent primis faucis</h4>
-                        <p>Cras aliquam arcu id diam pharetra malesuada. Vestibulum id pulvinar tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</p>
-                        <a href="#" class="read-lnk">read more</a>
-                    </article>
-                    <article>
-                        <h4 data-flex="-center"><i class="icon-blinklist"></i>vestibulim id tellus</h4>
-                        <p>Cras aliquam arcu id diam pharetra malesuada. Vestibulum id pulvinar tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</p>
-                        <a href="#" class="read-lnk">read more</a>
-                    </article>
-                    <article>
-                        <h4 data-flex="-center"><i class="icon-server"></i>torquent primis faucis</h4>
-                        <p>Cras aliquam arcu id diam pharetra malesuada. Vestibulum id pulvinar tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</p>
-                        <a href="#" class="read-lnk">read more</a>
-                    </article>  
+                        <article>
+                            <h4 data-flex="-center"><i class="icon-download"></i>torquent primis faucis</h4>
+                            <p>Cras aliquam arcu id diam pharetra malesuada. Vestibulum id pulvinar tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</p>
+                            <a href="#" class="read-lnk">read more</a>
+                        </article>
+                        <article>
+                            <h4 data-flex="-center"><i class="icon-blinklist"></i>vestibulim id tellus</h4>
+                            <p>Cras aliquam arcu id diam pharetra malesuada. Vestibulum id pulvinar tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</p>
+                            <a href="#" class="read-lnk">read more</a>
+                        </article>
+                        <article>
+                            <h4 data-flex="-center"><i class="icon-server"></i>torquent primis faucis</h4>
+                            <p>Cras aliquam arcu id diam pharetra malesuada. Vestibulum id pulvinar tellus. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</p>
+                            <a href="#" class="read-lnk">read more</a>
+                        </article>  
                     </div>
                 </div>
             </section>
@@ -115,42 +140,42 @@
         <footer>
             <section class="footer">
                 <div class="lwrap" data-flex="stripe w">
-                <div>
-                    <h5>Services</h5>
-                    <div class="links-footer" data-flex="col -start">
-                        <a href="#">Vestibulum Ante </a>
-                        <a href="#">Sipsum Primis in Faucibus</a>
-                        <a href="#">Orci Luctus et Ultrices</a>
-                        <a href="#">Posuere  at Sed Accusom</a>
-                        <a href="#">Magnus Lorem Quisit</a>
-                        <a href="#">Aenean id Dolroest Pursus</a>
-                        <a href="#">Nulla Quam Mollis</a>
-                    </div>
-                </div>
-                <div>
-                    <h5>Other Links & Information</h5>
-                    <div class="links-footer" data-flex="col -start">
-                        <a href="#">Vestibulum Ante </a>
-                        <a href="#">Sipsum Primis in Faucibus</a>
-                        <a href="#">Orci Luctus et Ultrices</a>
-                        <a href="#">Posuere  at Sed Accusom</a>
-                        <a href="#">Magnus Lorem Quisit</a>
-                        <a href="#">Aenean id Dolroest Pursus</a>
-                        <a href="#">Nulla Quam Mollis</a>
-                    </div>
-                </div>
-                <div>
-                    <h3>Questions for Us?</h3>
-                    <form action="#" method="post">
-                        <div data-flex="stripe">
-                            <input type="text" class="input-default" name="" placeholder="Subject">
-                            <input type="text" class="input-default" name="" placeholder="Subject">
+                    <div>
+                        <h5>Services</h5>
+                        <div class="links-footer" data-flex="col -start">
+                            <a href="#">Vestibulum Ante </a>
+                            <a href="#">Sipsum Primis in Faucibus</a>
+                            <a href="#">Orci Luctus et Ultrices</a>
+                            <a href="#">Posuere  at Sed Accusom</a>
+                            <a href="#">Magnus Lorem Quisit</a>
+                            <a href="#">Aenean id Dolroest Pursus</a>
+                            <a href="#">Nulla Quam Mollis</a>
                         </div>
-                        <textarea class="textarea-default" placeholder="Message"></textarea>
-                        <button><span class="btn btn-submit">Submit</span></button>
-                    </form>
+                    </div>
+                    <div>
+                        <h5>Other Links & Information</h5>
+                        <div class="links-footer" data-flex="col -start">
+                            <a href="#">Vestibulum Ante </a>
+                            <a href="#">Sipsum Primis in Faucibus</a>
+                            <a href="#">Orci Luctus et Ultrices</a>
+                            <a href="#">Posuere  at Sed Accusom</a>
+                            <a href="#">Magnus Lorem Quisit</a>
+                            <a href="#">Aenean id Dolroest Pursus</a>
+                            <a href="#">Nulla Quam Mollis</a>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>Questions for Us?</h3>
+                        <form action="#" method="post">
+                            <div data-flex="stripe">
+                                <input type="text" class="input-default" name="" placeholder="Subject">
+                                <input type="email" class="input-default" name="" placeholder="Email">
+                            </div>
+                            <textarea class="textarea-default" placeholder="Message"></textarea>
+                            <button><span class="btn btn-submit">Submit</span></button>
+                        </form>
+                    </div>
                 </div>
-            </div>
             </section>
             <section class="copyright">
                 <div class="lwrap" data-flex="axis">
@@ -160,8 +185,8 @@
                         <div class="links-social" data-flex="end w">
                             <a href="#"><i class="icon-tw"></i></a>
                             <a href="#"><i class="icon-in"></i></a>
-                            <a href="#"><i class="icon-fb"></i></a>
                             <a href="#"><i class="icon-fl"></i></a>
+                            <a href="#"><i class="icon-fb"></i></a>
                             <a href="#"><i class="icon-net"></i></a>
                         </div>
                     </div>
@@ -169,7 +194,19 @@
             </section>
         </footer>
         <script src="/js/jquery/jquery-2.1.4.min.js"></script>
+        <script src="/js/jquery/plugins/jQuery-Feature-Carousel/js/jquery.featureCarousel.min.js"></script>
         <script src="/js/tabs-accordions.js"></script>
         <script src="/js/index.js"></script>
+        <script>
+            $(document).ready(function () {
+                var carousel = $("#carousel").featureCarousel({
+                    // include options like this:
+                    // (use quotes only for string values, and no trailing comma after last option)
+                    // option: value,
+                    // option: value
+                    trackerSummation: false
+                });
+            });
+        </script>
     </body>
 </html>
