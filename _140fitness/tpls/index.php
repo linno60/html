628094d094664a690add604555e166c150ae81d5
Ivan <auzp@ukr.net>
2016-12-25 15:49:57 +0200

    commit

--- _140fitness/tpls/index.php
+++ _140fitness/tpls/index.php
@@ -44,12 +44,12 @@
                 </div>
             </header>
             <main>
-                <section class="features lwrap" data-flex="stripe w" data-childs='col between -start'>
+                <section class="features lwrap" data-flex="stripe w" data-childs='col between -start' data-flex-480="vstripe">
                     <article>
                         <div>
                             <i class="icon-case"></i>
                             <h4>Resources</h4>
-                            <p>Easily manage your inspiration and<br> work-in-progress by dragging images into projects and sharable client groups. Easily manage your inspiration and.</p>
+                            <p>Easily manage your inspiration and work-in-progress by dragging images into projects and sharable client groups. Easily manage your inspiration and.</p>
                         </div>
                         <a href="#">Explore more &rarr;</a>
                     </article>
@@ -57,7 +57,7 @@
                         <div>
                             <i class="icon-clock"></i>
                             <h4>Training & Funding</h4>
-                            <p>Easily manage your inspiration and<br> work-in-progress by dragging images into projects and sharable client groups.</p>
+                            <p>Easily manage your inspiration and work-in-progress by dragging images into projects and sharable client groups.</p>
                         </div>
                         <a href="#">Browse training &rarr;</a>
                     </article>
@@ -65,7 +65,7 @@
                         <div>
                             <i class="icon-cloud"></i>
                             <h4>Connect</h4>
-                            <p>Easily manage your inspiration and<br> work-in-progress by dragging images into projects and sharable client groups. Easily manage your inspiration.</p>
+                            <p>Easily manage your inspiration and work-in-progress by dragging images into projects and sharable client groups. Easily manage your inspiration.</p>
                         </div>
                         <a href="#">Connect now &rarr;</a>
                     </article>
@@ -73,80 +73,88 @@
                         <div>
                             <i class="icon-bubble"></i>
                             <h4>Communicate</h4>
-                            <p>Easily manage your inspiration and<br> work-in-progress by dragging images into projects and sharable client groups. Easily</p>
+                            <p>Easily manage your inspiration and work-in-progress by dragging images into projects and sharable client groups. Easily</p>
                         </div>
                         <a href="#">Start now &rarr;</a>
                     </article>
                 </section>
-                <section class="grid lwrap" data-flex="axis w" data-childs="col -center">
-                    <article>
-                        <hgroup class="hdr">
-                            <h6>it's all about</h6>
-                            <h2>fitness first</h2>
-                            <i class="icon-dropgray"></i>
-                        </hgroup>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
-                        <a href="#" class="more">read more</a>
-                    </article>
-                    <figure><img src="/images/img1.png" width="780" height="394" alt=""></figure>
-                    <figure><img src="/images/img2.png" width="780" height="394" alt=""></figure>
-                    <article>
-                        <hgroup class="hdr">
-                            <h6>love your</h6>
-                            <h2>your body</h2>
-                            <i class="icon-dropgray"></i>
-                        </hgroup>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
-                        <a href="#" class="more">read more</a>
-                    </article>
+                <section class="gallery-main grid lwrap" data-childs="axis" data-childs-480="vstripe">
+                    <div class="row">
+                        <article data-flex="col -center">
+                            <hgroup class="hdr">
+                                <h6>it's all about</h6>
+                                <h2>fitness first</h2>
+                                <i class="icon-dropgray"></i>
+                            </hgroup>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+                            <a href="#" class="more">read more</a>
+                        </article>
+                        <figure><img src="/images/img1.png" width="780" height="394" alt=""><span class="details"><a href="#">Active Users</a> <span>(34)</span><i class="icon-dropred"></i></span></figure>   
+                    </div>
+                    <div class="row">                        
+                        <article data-flex="col -center">
+                            <hgroup class="hdr">
+                                <h6>love your</h6>
+                                <h2>your body</h2>
+                                <i class="icon-dropgray"></i>
+                            </hgroup>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p>
+                            <a href="#" class="more">read more</a>
+                        </article>
+                        <figure><img src="/images/img2.png" width="780" height="394" alt=""><span class="details"><a href="#">Products USed</a> <span>(658)</span><i class="icon-dropred"></i></span></figure>
+                    </div>
                 </section>
             </main>
-            <section class='gallery grid lwrap' data-flex='stripe w' data-childs='col'>
-                <div>
-                    <figure><img src="/images/img3.png" width="401" height="330" alt=""></figure>
-                    <article>
-                        <hgroup class="hdr" data-flex='stripe'>
-                            <div>
-                                <h6>track your</h6>
-                                <h2>lifestyle</h2>
-                            </div>
-                            <i class="icon-user"></i>
-                        </hgroup>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p> 
-                    </article>
+            <section class='gallery-add grid lwrap' data-childs='stripe' data-childs-480="vstripe">
+                <div class="row" data-childs="col">
+                    <div>
+                        <figure><img src="/images/img3.png" width="401" height="330" alt=""></figure>
+                        <article>
+                            <hgroup class="hdr" data-flex='stripe'>
+                                <div>
+                                    <h6>track your</h6>
+                                    <h2>lifestyle</h2>
+                                </div>
+                                <i class="icon-user"></i>
+                            </hgroup>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p> 
+                        </article>
+                    </div>
+                    <div>
+                        <figure><img src="/images/img4.png" width="779" height="329" alt=""><span class="details"><time>24th august</time><a href="#">health & fitness<i class="icon-mark"></i></a></span></figure>
+                        <article>
+                            <h5>summer of fun</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p> 
+                        </article>
+                    </div> 
                 </div>
-                <div>
-                    <figure><img src="/images/img4.png" width="779" height="329" alt=""></figure>
-                    <article>
-                        <h5>summer of fun</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p> 
-                    </article>
-                </div>
-                <div>
-                    <figure><img src="/images/img5.png" width="779" height="359" alt=""></figure>
-                    <article>
-                        <hgroup class="hdr" data-flex='stripe'>
-                            <div>
-                                <h6>track your</h6>
-                                <h2>get involved in yourself</h2> 
-                            </div>
-                            <i class="icon-strawberry"></i>
-                        </hgroup>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillumdolore eu fugiat nulla pariatur.</p> 
-                    </article>
-                </div>
-                <div>
-                    <figure><img src="/images/img6.png" width="401" height="359" alt=""></figure>
-                    <article>
-                        <hgroup class="hdr" data-flex='stripe'>
-                            <div>
-                                <h6>improve your</h6>
-                                <h2>eating habits</h2>
-                            </div>
-                            <i class="icon-aiwa"></i>
-                        </hgroup>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p> 
-                    </article>
+                <div class="row" data-childs="col">
+                    <div>
+                        <figure><img src="/images/img6.png" width="401" height="359" alt=""></figure>
+                        <article>
+                            <hgroup class="hdr" data-flex='stripe'>
+                                <div>
+                                    <h6>improve your</h6>
+                                    <h2>eating habits</h2>
+                                </div>
+                                <i class="icon-aiwa"></i>
+                            </hgroup>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.</p> 
+                        </article>
+                    </div>
+                    <div>
+                        <figure><img src="/images/img5.png" width="779" height="359" alt=""></figure>
+                        <article>
+                            <hgroup class="hdr" data-flex='stripe'>
+                                <div>
+                                    <h6>track your</h6>
+                                    <h2>get involved in yourself</h2> 
+                                </div>
+                                <i class="icon-strawberry"></i>
+                            </hgroup>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillumdolore eu fugiat nulla pariatur.</p> 
+                        </article>
+                    </div>
                 </div>
             </section>
             <section class='banner'>
@@ -170,11 +178,11 @@
                     <div>
                         <h5>Stay in touch</h5>
                         <div class="links-social" data-flex="end w">
-                        <a href="#" class="fb"></a>
-                        <a href="#" class="tw"></a>
-                        <a href="#" class="yt"></a>
-                        <a href="#" class="in"></a>
-                    </div>
+                            <a href="#" class="fb"></a>
+                            <a href="#" class="tw"></a>
+                            <a href="#" class="yt"></a>
+                            <a href="#" class="in"></a>
+                        </div>
                     </div>
                     <form action='#' method='post' data-flex='stripe'>
                         <input type="text" name="form" class="input-default" placeholder="Your Name">
