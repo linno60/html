da02ddd5058761bc2b08935a5fa2fc0fa0bc6933
Ivan <auzp@ukr.net>
2017-01-12 21:19:16 +0200

    commit

--- _142food-restaurant/tpls/index.php
+++ _142food-restaurant/tpls/index.php
@@ -20,13 +20,13 @@
         </script>
     </head>
     <body>
-        <header>
+        <header data-flex="col">
             <section class="top-bar">
-                <div class="lwrap" data-flex="end">
-                    <div class="logo" data-flex="center">
-                       <a href="/" title="Return to the homepage" id="logo">
-                        <img src="/images/logo.png" width="167" height="87" alt="Food Restaurant">
-                    </a> 
+                <div class="lwrap" data-flex="axis">
+                    <div class="logo-wrap" data-flex="center">
+                        <a href="/" title="Return to the homepage" id="logo">
+                            <img src="/images/logo.png" width="167" height="87" alt="Food Restaurant">
+                        </a> 
                     </div>
                     <ul class="list-duty" data-childs="-center" data-flex="w">
                         <li><i class="icon-clocksm"></i>Order Foods 24/7</li>
@@ -55,16 +55,14 @@
                     <a href="#" class="active">table booking</a>   
                 </nav>
             </section>
-            <section class="banner">
-                <div class="lwrap">
-                    <h1>Foody love</h1>
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi. </p>
-                    <a href="#" class="btn btn-alt">Click here</a>
-                </div>
+            <section class="banner lwrap" data-flex="col center -center">
+                <h1>Foody love</h1>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi. </p>
+                <a href="#" class="btn btn-alt">Click here</a>
             </section>
         </header>
         <section class="about">
-            <div class="lwrap" data-flex="stripe">
+            <div class="lwrap">
                 <div class="figure">
                     <figure><img src="/images/aboutimg1.jpg" width="350" height="289" alt=""></figure>
                     <figure><img src="/images/aboutimg2.jpg" width="350" height="289" alt=""></figure>
@@ -74,8 +72,8 @@
                         <h2>About us</h2>
                         <h3>we are tasty</h3>
                     </hgroup>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stan</p>
-                    <p>dard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesettingdard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the indu&shy;stry's stan</p>
+                    <p>dard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic type&shy;set&shy;tingdard dummy text ever since the 1500s,when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting</p>
                     <a href="#" class="btn btn-default">Click here</a>
                 </article>
             </div>
@@ -142,7 +140,7 @@
                     <h2>Amazing</h2>
                     <h3>delicious</h3>
                 </hgroup>
-                <div data-childs="stripe">
+                <div data-childs="axis">
                     <div class="menu-item">
                         <figure><img src="/images/drinks.png" width="668" height="536" alt=""></figure>
                         <div>
@@ -276,33 +274,39 @@
             </div>
         </section>
         <section class="apps">
-            <div class="lwrap" data-flex="axis">
+            <div class="lwrap" data-flex="stripe">
                 <figure><img src="/images/iphone.png" width="271" height="553" alt=""></figure>
                 <div>
                     <hgroup>
                         <h6>Happy to Announce</h6>
                         <h2>mobile app</h2>
-                        <h5>is available in every os platform</h5>
+                        <h5>is available in every os platform.</h5>
                     </hgroup>
+                    <div data-flex="stripe w">
+                        <a href="#"><img src="/images/googleplay.png" width="243" height="84" alt=""></a>
+                        <a href="#"><img src="/images/appstore.png" width="269" height="84" alt=""></a>
+                    </div>
                 </div>
             </div>
         </section>  
         <footer>
             <section class="footer">
                 <div class="lwrap" data-flex="stripe">
-                    <div>
-                        <h5>Take it easy with location</h5>
-                        <address>112-Legere ancillae vix ne. Te elit putent propriae eum, aliquip nominati</address>
-                        <dl>
-                            <li>
-                            <dd>phone:</dd>
-                            <dt>00 000 000</dt>
-                            </li>
-                            <li>
-                            <dd>Email:</dd>
-                            <dt><a href="mailto:support@templatemela.com">support@templatemela.com</a></dt>
-                            </li>
-                        </dl>
+                    <div data-flex="vaxis" class="contacts">
+                        <div>
+                            <h5>Take it easy with location</h5>
+                            <address>112-Legere ancillae vix ne.<br> Te elit putent propriae eum,<br> aliquip nominati</address>
+                            <dl>
+                                <li>
+                                <dd>phone:</dd>
+                                <dt>00 000 000</dt>
+                                </li>
+                                <li>
+                                <dd>Email:</dd>
+                                <dt><a href="mailto:support@templatemela.com">support@templatemela.com</a></dt>
+                                </li>
+                            </dl> 
+                        </div>
                         <div class="links-social" data-flex="center -center w">
                             <a href="#"><i class="icon-fb"></i></a>
                             <a href="#"><i class="icon-gp"></i></a>
@@ -314,7 +318,7 @@
                             <a href="#"><i class="icon-ig"></i></a>
                         </div>
                     </div>
-                    <div>
+                    <div class="working">
                         <h5>Working Hours</h5>
                         <h6>Monday To Friday</h6>
                         <ul>
@@ -327,13 +331,14 @@
                             <li>11:00am to 12:00pm(Lunch/Dinner)</li>
                         </ul>
                     </div>
-                    <div>
+                    <div class="form-wrap">
                         <h5>Feedback form</h5>
                         <form action="#" method="post">
                             <input class="input-default" type="text" name="form" placeholder="Name">
                             <input class="input-default" type="email" name="form" placeholder="Email">
                             <input class="input-default" type="tel" name="form" placeholder="Phone">
                             <textarea class="textarea-default" placeholder="Message"></textarea>
+                            <button><span class="btn btn-default">Submit</span></button>
                         </form>
                     </div>
                 </div>
