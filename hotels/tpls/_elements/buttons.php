c2cae10b4362adf1db06533544e93e5a4bab881e
Ivan <auzp@ukr.net>
2017-03-10 11:50:59 +0200

    commit

--- hotels/tpls/_elements/buttons.php
+++ hotels/tpls/_elements/buttons.php
@@ -1,8 +1,8 @@
-<a href="/" class="btn btn-green"><i class="fa fa-envelope"></i>email</a>
-<a href="/" class="btn btn-green disabled"><i class="fa fa-envelope"></i>email</a>
+<span><a href="/" class="btn btn-green"><i class="fa fa-envelope"></i>email</a></span>
+<span class="disabled"><a href="/" class="btn btn-green"><i class="fa fa-envelope"></i>email</a></span>
 <button><span class="btn btn-default"><i class="fa fa-play-circle icon-blue"></i>learn to use</span></button>
 <a href="/" class="btn btn-default active">month</a>
-<button class="btn btn-default" data-tooltip="Refresh"><i class="fa fa-refresh"></i></button>
+<button><span class="btn btn-default" data-tooltip="Refresh"><i class="fa fa-refresh"></i></span></button>
 <a href="/" class="btn btn-blue">apply</a>
 <a href="/" class="btn btn-default edit-notes"><i class="fa fa-pencil"></i></a>
 <a href="/" class="btn btn-red sm">not setup, click to setup</a>
